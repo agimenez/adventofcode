d6480bd52f0ba0739defef8994b93f5d8b2b236e
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-04 22:10:02 +0100

    day03: shortcircuit logic

--- 2019/03/main.go
+++ 2019/03/main.go
@@ -27,40 +27,80 @@ func (m motion) String() string {
 }
 
 type circuit struct {
-	wired         map[point]bool
-	cableTip      point
+	wirings       []map[point]bool
+	wires         int
 	shortCircuits map[point]bool
 }
 
-func (c *circuit) runWiring(wire []motion) {
-	dbg("*** Running new wiring ***")
-	c.cableTip = point{0, 0}
-
-	for _, m := range wire {
-		c.doMotion(m)
+func newCircuit() *circuit {
+	return &circuit{
+		wirings:       []map[point]bool{},
+		wires:         0,
+		shortCircuits: make(map[point]bool, 100),
 	}
 }
 
-func (c *circuit) doMotion(m motion) {
-	dbg("Motion: %c -> %d", m.dir, m.length)
+func (c *circuit) runWiring(wire []motion) {
+	dbg("*** Running new wiring %d ***", c.wires)
+	cableTip := point{0, 0}
+
+	//c.wiring[c.wires] = make(map[point]bool, len(wire))
+	c.wirings = append(c.wirings, map[point]bool{})
+
+	for _, m := range wire {
+		cableTip = c.doMotion(m, cableTip)
+	}
+
+	c.wires++
+	c.checkShortCircuits()
+}
+
+// checkShortCircuits matches the last wiring against the previous ones to find short
+// circuits
+func (c *circuit) checkShortCircuits() {
+	if len(c.wirings) < 2 {
+		return
+	}
+
+	topWire := c.wirings[len(c.wirings)-1]
+
+	for i, wiring := range c.wirings[:len(c.wirings)-1] {
+		dbg("Checking shortcircuits %d/%d", i, len(c.wirings)-1)
+		for point := range topWire {
+			dbg(" -> Point %v", point)
+			if wiring[point] {
+				dbg("   *** MATCH! ***")
+				c.shortCircuits[point] = true
+			} else {
+				dbg("   (nope)")
+			}
+		}
+	}
+
+}
+
+func (c *circuit) doMotion(m motion, cableTip point) point {
+	dbg(" Motion Wire %d: %c -> %d", c.wires, m.dir, m.length)
 
 	for i := 0; i < m.length; i++ {
 		switch m.dir {
 		case 'U':
-			c.cableTip.y++
+			cableTip.y++
 		case 'D':
-			c.cableTip.y--
+			cableTip.y--
 		case 'L':
-			c.cableTip.x--
+			cableTip.x--
 		case 'R':
-			c.cableTip.x++
+			cableTip.x++
 		default:
 			panic(fmt.Sprintf("Bad input motion direction %c", m.dir))
 		}
-		c.wired[c.cableTip] = true
-		dbg("  -> New cableTip: %v", c.cableTip)
+
+		c.wirings[c.wires][cableTip] = true
+		//dbg("  -> New cableTip: %v", c.cableTip)
 	}
 
+	return cableTip
 }
 
 func dbg(fmt string, v ...interface{}) {
@@ -82,10 +122,7 @@ func main() {
 	w1 := parseWiring(wire1)
 	w2 := parseWiring(wire2)
 
-	c := circuit{
-		wired:    make(map[point]bool, len(w1)),
-		cableTip: point{0, 0},
-	}
+	c := newCircuit()
 	c.runWiring(w1)
 	c.runWiring(w2)
 
