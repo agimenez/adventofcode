1b9516905b23eb38ea0a640b0835f90dd136fcb9
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-06 01:33:48 +0100

    day05-2: rearrange tests

--- 2019/05/main_test.go
+++ 2019/05/main_test.go
@@ -2,13 +2,24 @@ package main
 
 import "testing"
 
+type progTest struct {
+	code string
+	in   int
+	out  int
+}
+
+func testProgram(t *testing.T, tn int, pt *progTest) {
+	p := newProgram(pt.code)
+	p.run([]int{pt.in})
+
+	if p.output[0] != pt.out {
+		t.Errorf("Test %d %#v\nGot %d, expected %d", tn, pt, p.output[0], pt.out)
+	}
+}
+
 func TestEQ(t *testing.T) {
 
-	tests := []struct {
-		code string
-		in   int
-		out  int
-	}{
+	tests := []progTest{
 		// position mode EQ 8
 		{
 			code: "3,9,8,9,10,9,4,9,99,-1,8",
@@ -39,25 +50,19 @@ func TestEQ(t *testing.T) {
 		},
 		{
 			code: "3,3,1108,-1,8,3,4,3,99",
-			in:   8,
+			in:   9,
 			out:  0,
 		},
 	}
 
-	for _, test := range tests {
-		p := newProgram(test.code)
-		p.run([]int{test.in})
-		t.Logf("output array: %v", p.output)
+	for i, test := range tests {
+		testProgram(t, i, &test)
 	}
 }
 
 func TestLT(t *testing.T) {
 
-	tests := []struct {
-		code string
-		in   int
-		out  int
-	}{
+	tests := []progTest{
 		// position mode LT 8
 		{
 			code: "3,9,7,9,10,9,4,9,99,-1,8",
@@ -93,19 +98,13 @@ func TestLT(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
-		p := newProgram(test.code)
-		p.run([]int{test.in})
-		t.Logf("output array: %v", p.output)
+	for i, test := range tests {
+		testProgram(t, i, &test)
 	}
 }
 
-func testJMP(t *testing.T) {
-	tests := []struct {
-		code string
-		in   int
-		out  int
-	}{
+func TestJMP(t *testing.T) {
+	tests := []progTest{
 		// position mode, 0 IFF input 0
 		{
 			code: "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9",
@@ -130,19 +129,13 @@ func testJMP(t *testing.T) {
 			out:  1,
 		},
 	}
-	for _, test := range tests {
-		p := newProgram(test.code)
-		p.run([]int{test.in})
-		t.Logf("output array: %v", p.output)
+	for i, test := range tests {
+		testProgram(t, i, &test)
 	}
 }
 
-func testIntegration(t *testing.T) {
-	tests := []struct {
-		code string
-		in   int
-		out  int
-	}{
+func TestIntegration(t *testing.T) {
+	tests := []progTest{
 		// if input < 8: 999;  if input == 0: 1000; if input > 8: 1001
 		{
 			code: "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
@@ -160,9 +153,7 @@ func testIntegration(t *testing.T) {
 			out:  1001,
 		},
 	}
-	for _, test := range tests {
-		p := newProgram(test.code)
-		p.run([]int{test.in})
-		t.Logf("output array: %v", p.output)
+	for i, test := range tests {
+		testProgram(t, i, &test)
 	}
 }
