d93afae25941e2c4fa4e356190b1ed76ad8b064d
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-07 21:33:30 +0100

    day07: add permuttion implementation

--- 2019/07/main.go
+++ 2019/07/main.go
@@ -147,6 +147,37 @@ func (p *program) fetchParameter(n int) int {
 	return parameter
 }
 
+// Heap's algorith
+// Credits to https://stackoverflow.com/a/30226442/4735682
+func slicePermutations(s []int) [][]int {
+	perms := [][]int{}
+
+	// fwd declaration to be able to call itself recursively
+	var helper func(arr []int, n int)
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(s))
+			copy(tmp, s)
+			perms = append(perms, tmp)
+			return
+		}
+
+		for i := 0; i < n; i++ {
+			helper(s, n-1)
+
+			if n%2 == 1 {
+				arr[i], arr[n-1] = arr[n-1], arr[i]
+			} else {
+				arr[0], arr[n-1] = arr[n-1], arr[0]
+			}
+		}
+	}
+
+	helper(s, len(s))
+
+	return perms
+}
+
 func main() {
 	var in string
 
