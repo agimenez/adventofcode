a5750943cdfa62ee0f06248e50c28ada88696a26
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-09 19:33:20 +0100

    day07: enhance debugging stuff

--- 2019/07/main.go
+++ 2019/07/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math"
@@ -8,12 +9,12 @@ import (
 	"strings"
 )
 
-const (
-	debug = false
+var (
+	debug int
 )
 
-func dbg(fmt string, v ...interface{}) {
-	if debug {
+func dbg(level int, fmt string, v ...interface{}) {
+	if debug >= level {
 		log.Printf(fmt, v...)
 	}
 }
@@ -46,44 +47,44 @@ func newProgram(p string) *program {
 func (p *program) run(input <-chan int, output chan<- int) {
 
 	for op := p.mem[p.pc]; op != 99; {
-		dbg("MEM = %v", p.mem)
-		dbg("pc = %d; op = %v", p.pc, op)
+		dbg(3, "MEM = %v", p.mem)
+		dbg(3, "pc = %d; op = %v", p.pc, op)
 
 		opcode := op % 100
-		dbg("opcode = %v", opcode)
+		dbg(3, "opcode = %v", opcode)
 
 		switch opcode {
 		case 1: // Add
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+4])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			a, b, c := p.fetchParameter(1), p.fetchParameter(2), p.mem[p.pc+3]
-			dbg(" ADD %d %d -> %d", a, b, c)
+			dbg(3, " ADD %d %d -> %d", a, b, c)
 			p.mem[c] = a + b
 			p.pc += 4
 		case 2: // Mul
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+4])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			a, b, c := p.fetchParameter(1), p.fetchParameter(2), p.mem[p.pc+3]
-			dbg(" MUL %d %d -> %d", a, b, c)
+			dbg(3, " MUL %d %d -> %d", a, b, c)
 			p.mem[c] = a * b
 			p.pc += 4
 		case 3: // In
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+2])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+2])
 			var in, dst int
 			in, dst = <-input, p.mem[p.pc+1]
-			dbg(" IN  %d -> %d", in, dst)
+			dbg(3, " IN  %d -> %d", in, dst)
 			p.mem[dst] = in
 			p.pc += 2
 		case 4: // Out
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+2])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+2])
 			src := p.fetchParameter(1)
-			dbg(" OUT %d", src)
+			dbg(3, " OUT %d", src)
 
 			output <- src
 			p.pc += 2
 
 		case 5: // JMP IF TRUE
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+3])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+3])
 			tst, newpc := p.fetchParameter(1), p.fetchParameter(2)
-			dbg(" JMP %d if %d != 0", newpc, tst)
+			dbg(3, " JMP %d if %d != 0", newpc, tst)
 			if tst != 0 {
 				p.pc = newpc
 			} else {
@@ -91,9 +92,9 @@ func (p *program) run(input <-chan int, output chan<- int) {
 			}
 
 		case 6: // JMP IF FALSE
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+3])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+3])
 			tst, newpc := p.fetchParameter(1), p.fetchParameter(2)
-			dbg(" JMP %d if %d == 0", newpc, tst)
+			dbg(3, " JMP %d if %d == 0", newpc, tst)
 			if tst == 0 {
 				p.pc = newpc
 			} else {
@@ -101,9 +102,9 @@ func (p *program) run(input <-chan int, output chan<- int) {
 			}
 
 		case 7: // LT
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+4])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			first, second, dst := p.fetchParameter(1), p.fetchParameter(2), p.mem[p.pc+3]
-			dbg(" LT %d %d %d", first, second, dst)
+			dbg(3, " LT %d %d %d", first, second, dst)
 			if first < second {
 				p.mem[dst] = 1
 			} else {
@@ -113,9 +114,9 @@ func (p *program) run(input <-chan int, output chan<- int) {
 			p.pc += 4
 
 		case 8: // EQ
-			dbg(" INSTR = %v", p.mem[p.pc:p.pc+4])
+			dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			first, second, dst := p.fetchParameter(1), p.fetchParameter(2), p.mem[p.pc+3]
-			dbg(" EQ %d %d %d", first, second, dst)
+			dbg(3, " EQ %d %d %d", first, second, dst)
 			if first == second {
 				p.mem[dst] = 1
 			} else {
@@ -126,7 +127,7 @@ func (p *program) run(input <-chan int, output chan<- int) {
 		default:
 			log.Fatalf("Bad opcode = %v", op)
 		}
-		dbg("MEM = %v", p.mem)
+		dbg(3, " MEM = %v", p.mem)
 
 		op = p.mem[p.pc]
 	}
@@ -137,7 +138,7 @@ func (p *program) fetchParameter(n int) int {
 	parameter := p.mem[p.pc+n]
 	mode := opcode / int(math.Pow10(n+1)) % 10
 
-	dbg("  (fetch) param[%d](%d) mode: %d", n, parameter, mode)
+	dbg(3, "  (fetch) param[%d](%d) mode: %d", n, parameter, mode)
 	if mode == 0 {
 		return p.mem[parameter]
 	}
@@ -176,7 +177,13 @@ func slicePermutations(s []int) [][]int {
 	return perms
 }
 
+func init() {
+	flag.IntVar(&debug, "debug", 0, "debug level")
+	flag.Parse()
+}
+
 func main() {
+
 	var in string
 	fmt.Scan(&in)
 
@@ -184,7 +191,7 @@ func main() {
 	perms := slicePermutations([]int{0, 1, 2, 3, 4})
 	for _, perm := range perms {
 		thrustersSignal := runPermutation(&in, perm)
-		log.Printf("Got signal from perm %v: %v", perm, thrustersSignal)
+		dbg(1, "Got signal from perm %v: %v", perm, thrustersSignal)
 		if thrustersSignal > maxOutput {
 			maxOutput = thrustersSignal
 		}
