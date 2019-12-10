737d6baba7b866de3b4df1dbad23cd0a57a670f0
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-10 19:12:17 +0100

    day07-2: add tests

--- 2019/07/main_test.go
+++ 2019/07/main_test.go
@@ -196,5 +196,27 @@ func TestIntegration(t *testing.T) {
 }
 
 func TestAmps(t *testing.T) {
+	tests := []struct {
+		settings []int
+		program  string
+		max      int
+	}{
+		{
+			settings: []int{9, 8, 7, 6, 5},
+			program:  "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
+			max:      139629729,
+		},
+		{
+			settings: []int{9, 7, 8, 5, 6},
+			program:  "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
+			max:      18216,
+		},
+	}
 
+	for _, test := range tests {
+		max := runPermutation(test.program, test.settings)
+		if max != test.max {
+			t.Errorf("Got %d, expected %d", max, test.max)
+		}
+	}
 }
