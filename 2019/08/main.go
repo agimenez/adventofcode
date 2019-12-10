75a289ad4503b7d1ab2a30192d230cf3a2b1a32d
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-10 20:33:16 +0100

    day08: initial

--- 2019/08/main.go
+++ 2019/08/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
+	"strings"
 )
 
 var (
@@ -25,7 +27,27 @@ func main() {
 
 	var in string
 	fmt.Scan(&in)
+	wide := 25
+	tall := 6
 
-	log.Printf("Max output: %v", in)
+	minz := math.MaxInt32
+	res := 0
+	for start, end := 0, wide*tall; start < len(in); start, end = end, end+(wide*tall) {
+		dbg(1, "start = %d, end = %d", start, end)
+		str := in[start:end]
+		z := strings.Count(str, "0")
+		dbg(2, "str = %s, z = %d (%d)", str, z, minz)
+		if z < minz {
+			o := strings.Count(str, "1")
+			t := strings.Count(str, "2")
+			dbg(2, "new max! o = %d, t = %d", o, t)
+
+			minz = z
+			res = o * t
+		}
+
+	}
+
+	log.Printf("Max output: %v", res)
 
 }
