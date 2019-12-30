f48ea39f36892c1a96e1120dc5eea2d1b3ab20a2
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-30 13:29:27 +0100

    intcode: raise debug levels

--- 2019/intcode/intcode.go
+++ 2019/intcode/intcode.go
@@ -48,58 +48,58 @@ func (p *Program) ensureAddr(addr int) {
 func (p *Program) SetMem(addr, val int) {
 	p.ensureAddr(addr)
 
-	utils.Dbg(3, "  (SetMem) p.mem[%d] = %d", addr, val)
+	utils.Dbg(4, "  (SetMem) p.mem[%d] = %d", addr, val)
 	p.mem[addr] = val
 }
 
 func (p *Program) getMem(addr int) int {
 	p.ensureAddr(addr)
 
-	utils.Dbg(3, "  (get) p.mem[%d] = %d", addr, p.mem[addr])
+	utils.Dbg(4, "  (get) p.mem[%d] = %d", addr, p.mem[addr])
 	return p.mem[addr]
 }
 func (p *Program) Run(input <-chan int, output chan<- int) {
 
 	p.pc = 0
 	for op := p.mem[p.pc]; op != 99; {
-		utils.Dbg(4, "MEM = %v", p.mem)
-		utils.Dbg(2, "pc = %d; op = %v", p.pc, op)
+		utils.Dbg(5, "MEM = %v", p.mem)
+		utils.Dbg(3, "pc = %d; op = %v", p.pc, op)
 
 		opcode := op % 100
-		utils.Dbg(2, "opcode = %v", opcode)
+		utils.Dbg(3, "opcode = %v", opcode)
 
 		switch opcode {
 		case 1: // Add
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+4])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			a, b, c := p.fetchParameter(1), p.fetchParameter(2), p.getAddrIndex(3)
-			utils.Dbg(2, " ADD %d %d -> %d", a, b, c)
+			utils.Dbg(3, " ADD %d %d -> %d", a, b, c)
 			p.SetMem(c, a+b)
 			p.pc += 4
 		case 2: // Mul
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+4])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			a, b, c := p.fetchParameter(1), p.fetchParameter(2), p.getAddrIndex(3)
-			utils.Dbg(2, " MUL %d %d -> %d", a, b, c)
+			utils.Dbg(3, " MUL %d %d -> %d", a, b, c)
 			p.SetMem(c, a*b)
 			p.pc += 4
 		case 3: // In
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+2])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+2])
 			var in, dst int
 			in, dst = <-input, p.getAddrIndex(1)
-			utils.Dbg(2, " IN  %d -> mem[%d]", in, dst)
+			utils.Dbg(3, " IN  %d -> mem[%d]", in, dst)
 			p.SetMem(dst, in)
 			p.pc += 2
 		case 4: // Out
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+2])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+2])
 			src := p.fetchParameter(1)
-			utils.Dbg(2, " OUT %d", src)
+			utils.Dbg(3, " OUT %d", src)
 
 			output <- src
 			p.pc += 2
 
 		case 5: // JMP IF TRUE
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+3])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+3])
 			tst, newpc := p.fetchParameter(1), p.fetchParameter(2)
-			utils.Dbg(2, " JMP %d if %d != 0", newpc, tst)
+			utils.Dbg(3, " JMP %d if %d != 0", newpc, tst)
 			if tst != 0 {
 				p.pc = newpc
 			} else {
@@ -107,9 +107,9 @@ func (p *Program) Run(input <-chan int, output chan<- int) {
 			}
 
 		case 6: // JMP IF FALSE
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+3])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+3])
 			tst, newpc := p.fetchParameter(1), p.fetchParameter(2)
-			utils.Dbg(2, " JMP %d if %d == 0", newpc, tst)
+			utils.Dbg(3, " JMP %d if %d == 0", newpc, tst)
 			if tst == 0 {
 				p.pc = newpc
 			} else {
@@ -117,9 +117,9 @@ func (p *Program) Run(input <-chan int, output chan<- int) {
 			}
 
 		case 7: // LT
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+4])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			first, second, dst := p.fetchParameter(1), p.fetchParameter(2), p.getAddrIndex(3)
-			utils.Dbg(2, " LT %d %d %d", first, second, dst)
+			utils.Dbg(3, " LT %d %d %d", first, second, dst)
 			if first < second {
 				p.SetMem(dst, 1)
 			} else {
@@ -129,9 +129,9 @@ func (p *Program) Run(input <-chan int, output chan<- int) {
 			p.pc += 4
 
 		case 8: // EQ
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+4])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+4])
 			first, second, dst := p.fetchParameter(1), p.fetchParameter(2), p.getAddrIndex(3)
-			utils.Dbg(2, " EQ %d %d %d", first, second, dst)
+			utils.Dbg(3, " EQ %d %d %d", first, second, dst)
 			if first == second {
 				p.SetMem(dst, 1)
 			} else {
@@ -140,9 +140,9 @@ func (p *Program) Run(input <-chan int, output chan<- int) {
 			p.pc += 4
 
 		case 9: // RELBASE
-			utils.Dbg(2, " INSTR = %v", p.mem[p.pc:p.pc+2])
+			utils.Dbg(3, " INSTR = %v", p.mem[p.pc:p.pc+2])
 			offset := p.fetchParameter(1)
-			utils.Dbg(2, " RELBASE  %d", offset)
+			utils.Dbg(3, " RELBASE  %d", offset)
 
 			p.base += offset
 			p.pc += 2
@@ -150,7 +150,7 @@ func (p *Program) Run(input <-chan int, output chan<- int) {
 		default:
 			log.Fatalf("Bad opcode = %v", op)
 		}
-		utils.Dbg(4, " MEM = %v", p.mem)
+		utils.Dbg(5, " MEM = %v", p.mem)
 
 		op = p.mem[p.pc]
 	}
@@ -178,21 +178,21 @@ func (p *Program) getAddrIndex(n int) int {
 func (p *Program) fetchParameter(n int) int {
 	mode := p.instructionMode(n)
 	parameter := p.mem[p.pc+n]
-	utils.Dbg(3, "   param[%d](%d) mode: %d, base %d, memsize %d", n, parameter, mode, p.base, len(p.mem))
+	utils.Dbg(4, "   param[%d](%d) mode: %d, base %d, memsize %d", n, parameter, mode, p.base, len(p.mem))
 
 	if mode == 0 {
 		// position mode
 		val := p.getMem(parameter)
-		utils.Dbg(3, "   (posmode) -> mem[%d] = %d", parameter, val)
+		utils.Dbg(4, "   (posmode) -> mem[%d] = %d", parameter, val)
 		return val
 	} else if mode == 2 {
 		// relative mode
 		val := p.getMem(p.base + parameter)
-		utils.Dbg(3, "   (relmode) -> mem[%d+%d] = %d", p.base, parameter, val)
+		utils.Dbg(4, "   (relmode) -> mem[%d+%d] = %d", p.base, parameter, val)
 		return val
 	}
 
 	// immediate mode
-	utils.Dbg(3, "   (immmode) -> = %d", parameter)
+	utils.Dbg(4, "   (immmode) -> = %d", parameter)
 	return parameter
 }
