1222bc1c0440c7295bbd2d571c4aaf2a8613d334
L. Alberto Giménez <alberto.gimenez@capside.com>
2019-12-30 20:01:21 +0100

    day17-2: done

--- 2019/17/main.go
+++ 2019/17/main.go
@@ -128,6 +128,30 @@ func DrawImage(img []string) {
 	}
 }
 
+func (r *Robot) DrawAreaAround(l Location) {
+	if utils.Debug == 0 {
+		return
+	}
+
+	fmt.Printf("len y = %d, len x = %d\n", len(r.image), len(r.image[0]))
+	fmt.Printf("    x = %d (@ = %d)\n", l.coord.x-3, l.coord.x)
+	for y := l.coord.y - 3; y < l.coord.y+3; y++ {
+		fmt.Printf("%4d", y)
+		for x := l.coord.x - 3; x < l.coord.x+3; x++ {
+			if l.coord.x == x && l.coord.y == y {
+				fmt.Print("@")
+			} else {
+				if y < 0 || y >= len(r.image) || x < 0 || x >= len(r.image[y]) {
+					fmt.Print("X")
+				} else {
+					fmt.Printf("%c", r.image[y][x])
+				}
+			}
+		}
+		fmt.Println()
+	}
+}
+
 type Location struct {
 	coord Point
 	dir   Direction
@@ -162,27 +186,36 @@ func (r *Robot) Location() Location {
 }
 
 func (l Location) Forward() Location {
-	return Location{Point{l.coord.x + l.dir.x, l.coord.y + l.dir.y}, l.dir}
+	utils.Dbg(2, " -> PREFWD: %+v", l)
+	l.coord.x += l.dir.x
+	l.coord.y += l.dir.y
+	utils.Dbg(2, " -> FWD: %+v", l)
+
+	return l
 }
 
 func (l Location) Valid(img []string) bool {
-	if l.coord.y >= len(img) {
+	utils.Dbg(2, " -> Valid: %+v", l)
+	if l.coord.y < 0 || l.coord.y >= len(img) {
+		utils.Dbg(2, " -> y out of bounds")
 		return false
 	}
 
-	if l.coord.x >= len(img[0]) {
+	if l.coord.x < 0 || l.coord.x >= len(img[0]) {
+		utils.Dbg(2, " -> x out of bounds")
 		return false
 	}
 
 	if img[l.coord.y][l.coord.x] != '#' {
+		utils.Dbg(2, " -> img[%d][%d] = %c", l.coord.y, l.coord.x, img[l.coord.y][l.coord.x])
 		return false
 	}
 
+	utils.Dbg(2, "   -> FOUND (%c)", img[l.coord.y][l.coord.x])
 	return true
 }
 
 func (l Location) Right() Location {
-	nl := l
 	switch l.dir {
 	case dirN:
 		l.dir = dirE
@@ -192,13 +225,15 @@ func (l Location) Right() Location {
 		l.dir = dirW
 	case dirW:
 		l.dir = dirN
+	default:
+		panic("Can't get new direction")
 	}
+	utils.Dbg(2, " -> Right: New direction: %+v", l)
 
-	return nl
+	return l
 }
 
 func (l Location) Left() Location {
-	nl := l
 	switch l.dir {
 	case dirN:
 		l.dir = dirW
@@ -208,43 +243,76 @@ func (l Location) Left() Location {
 		l.dir = dirE
 	case dirW:
 		l.dir = dirS
+	default:
+		panic("Can't get new direction")
+	}
+	utils.Dbg(2, " -> Left: New direction: %+v", l)
+
+	return l
+}
+
+func (l Location) Turn(dir rune) Location {
+
+	switch dir {
+	case 'R':
+		l = l.Right()
+	case 'L':
+		l = l.Left()
 	}
 
-	return nl
+	return l
 }
 func (r *Robot) FindDirection(l Location) rune {
+	utils.Dbg(2, " FindDirection: test FWD")
 	if l.Forward().Valid(r.image) {
+		utils.Dbg(1, "l.Forward() = %v", l.Forward())
 		return 'F'
 	}
 
+	utils.Dbg(2, " FindDirection: test Right")
 	if l.Right().Forward().Valid(r.image) {
 		return 'R'
 	}
 
+	utils.Dbg(2, " FindDirection: test Left")
 	if l.Left().Forward().Valid(r.image) {
 		return 'L'
 	}
 
-	// should not get here
-	return 'F'
+	// Halt
+	return 'H'
 }
 
 func (r *Robot) RunScaffolding(l Location) string {
 	var b strings.Builder
 
+	utils.Dbg(1, "Starting search form location %v", l)
 	fwd := 0
 	turn := r.FindDirection(l)
+	l = l.Turn(turn)
 	b.WriteRune(turn)
+	b.WriteRune(',')
+
 	for {
+		utils.Dbg(1, "Path: '%s', checking FWD", b.String())
+		r.DrawAreaAround(l)
 		for l.Forward().Valid(r.image) {
 			l = l.Forward()
+			utils.Dbg(1, "NEW location: %+v", l)
 			fwd++
+			r.DrawAreaAround(l)
 		}
-		b.WriteString(fmt.Sprintf("%d", fwd))
+		b.WriteString(fmt.Sprintf("%d,", fwd))
 		fwd = 0
 		turn := r.FindDirection(l)
-		b.WriteRune(turn)
+		if turn == 'H' {
+			break
+		}
+		l = l.Turn(turn)
+		b.WriteString(fmt.Sprintf("%c,", turn))
 	}
+
+	return b.String()
 }
 func (r *Robot) RunPartTwo() int {
 	go func() {
@@ -252,11 +320,19 @@ func (r *Robot) RunPartTwo() int {
 		r.cpu.Run(r.input, r.output)
 	}()
 
+	// This part I did manually, discovering the path using Robot.RunScaffolding(), since right now I'm not feeling like implementing a
+	// simple dictionary based compressor
+	// R,12,L,10,L,10,L,6,L,12,R,12,L,4,R,12,L,10,L,10,L,6,L,12,R,12,L,4,L,12,R,12,L,6,L,6,L,12,R,12,L,4,L,12,R,12,L,6,R,12,L,10,L,10,L,12,R,12,L,6,L,12,R,12,L,6
+	// Main: A,B,A,B,C,B,C,A,C,C
+	// A=R,12,L,10,L,10
+	// B=L,6,L,12,R,12,L,4
+	// C=L,12,R,12,L,6
+
 	inputs := []string{
-		"A,B,C",
-		"R,12,L,12",
-		"R,12,L,12",
-		"R,12,L,12",
+		"A,B,A,B,C,B,C,A,C,C",
+		"R,12,L,10,L,10",
+		"L,6,L,12,R,12,L,4",
+		"L,12,R,12,L,6",
 		"n",
 	}
 
@@ -264,8 +340,10 @@ func (r *Robot) RunPartTwo() int {
 	// is just a snapshot of the current camera status. Then it asks for the values,
 	// using a prompt
 	r.ReadCam()
+	//DrawImage(r.image)
 	p := r.Location()
-	r.RunScaffolding(p)
+	path := r.RunScaffolding(p)
+	fmt.Printf("Part two path: %s\n", path)
 
 	for _, cmd := range inputs {
 		prompt := r.ReadLine()
@@ -274,7 +352,13 @@ func (r *Robot) RunPartTwo() int {
 		utils.Dbg(1, "< %s", cmd)
 	}
 
-	return <-r.output
+	for {
+		c := <-r.output
+		utils.Dbg(1, "Got output: %c (%d)", c, c)
+		if c > 127 {
+			return c
+		}
+	}
 }
 func mod(a, b int) int {
 	return (a%b + b) % b
@@ -286,14 +370,15 @@ func main() {
 	fmt.Scan(&in)
 
 	r := newRobot(in)
-	//r.Run()
-	r.Paint()
+	r.Run()
+	//r.Paint()
 	part1 := r.SumAlignmentParameters()
 	fmt.Printf("Part one: %#v\n", part1)
 
 	// reset program
 	r = newRobot(in)
 	dust := r.RunPartTwo()
+
 	fmt.Printf("Part two: %#v\n", dust)
 
 }
