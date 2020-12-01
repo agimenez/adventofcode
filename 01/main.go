2da2de72749346faba94ee06a895f2a7139735a0
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-01 18:37:06 +0100

    day 1: part one done

--- 01/main.go
+++ 01/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"log"
+	"os"
+	"sort"
+	"strconv"
 )
 
 const (
@@ -16,9 +19,25 @@ func dbg(fmt string, v ...interface{}) {
 }
 
 func main() {
-	var in string
+	s := bufio.NewScanner(os.Stdin)
+	nums := []int{}
+	for s.Scan() {
+		l := s.Text()
+		n, _ := strconv.Atoi(l)
+		nums = append(nums, n)
+	}
 
-	fmt.Scan(&in)
+	sort.Ints(nums)
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] > 2020 {
+				break
+			}
 
-	log.Printf("%v", in)
+			if nums[i]+nums[j] == 2020 {
+				log.Printf("%v", nums[i]*nums[j])
+				os.Exit(0)
+			}
+		}
+	}
 }
