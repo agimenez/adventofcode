e62e81ce6160ba96df9ab6ce07a1c699e5a183be
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-04 16:52:26 +0100

    day 4: part one

--- 04/main.go
+++ 04/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"log"
+	"os"
+	"strings"
 )
 
 const (
@@ -15,10 +17,55 @@ func dbg(fmt string, v ...interface{}) {
 	}
 }
 
+type passport map[string]string
+
+func validPassport(p passport) bool {
+
+	if len(p) == 8 {
+		return true
+	}
+
+	if _, hasCid := p["cid"]; len(p) == 7 && !hasCid {
+		return true
+	}
+
+	return false
+}
+
 func main() {
-	var in string
 
-	fmt.Scan(&in)
+	s := bufio.NewScanner(os.Stdin)
+	list := []passport{}
 
-	log.Printf("%v", in)
+	p := passport{}
+	for s.Scan() {
+		l := s.Text()
+		dbg("Line: %v\n", l)
+		if l == "" {
+			list = append(list, p)
+			p = passport{}
+			continue
+		}
+
+		fields := strings.Split(l, " ")
+		for _, field := range fields {
+			f := strings.Split(field, ":")
+			p[f[0]] = f[1]
+		}
+
+		dbg("List: %v\n", list)
+
+	}
+	list = append(list, p)
+
+	part1 := 0
+	for _, p := range list {
+		if validPassport(p) {
+			part1++
+		}
+	}
+
+	log.Printf("Part 1: %v", part1)
+	log.Printf("Part 2: \n")
+
 }
