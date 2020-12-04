eebe78b848de5f607b12f73780246d61c36d0dc5
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-04 15:54:26 +0100

    day 3: part 1

--- 03/main.go
+++ 03/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	debug = true
+	debug = false
 )
 
 func dbg(fmt string, v ...interface{}) {
@@ -16,14 +16,65 @@ func dbg(fmt string, v ...interface{}) {
 	}
 }
 
-func main() {
-	s := bufio.NewScanner(os.Stdin)
+type Point struct {
+	x, y int
+}
+
+type slope struct {
+	width  int
+	height int
+	grid   map[Point]struct{}
+}
+
+func newSlope(s *bufio.Scanner) *slope {
+	g := &slope{
+		width:  0,
+		height: 0,
+		grid:   make(map[Point]struct{}),
+	}
+
 	for s.Scan() {
 		l := s.Text()
+		dbg("Line: '%v'\n", l)
+		g.width = len(l)
+
+		for x, square := range l {
+			if square == '#' {
+				g.grid[Point{x, g.height}] = struct{}{}
+
+			}
+		}
+		g.height++
+		dbg("After parsing: W: %v, H: %v\n", g.width, g.height)
 
 	}
 
-	log.Printf("Part 1: \n")
+	return g
+}
+
+func (s *slope) countTrees(v Point) int {
+	cur := Point{0, 0}
+	count := 0
+	for i := 0; i <= s.height; i++ {
+		if _, ok := s.grid[cur]; ok {
+			dbg("Tree at %v!\n", cur)
+			count++
+		}
+
+		cur.x = (cur.x + v.x) % s.width
+		cur.y = (cur.y + v.y)
+		dbg("New cur: %v\n", cur)
+	}
+
+	return count
+}
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	g := newSlope(s)
+	part1 := g.countTrees(Point{3, 1})
+
+	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: \n")
 
 }
