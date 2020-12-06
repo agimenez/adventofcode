1f51c7c739ba64addb5f3f25b0d51ae28ae421f3
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-06 12:01:04 +0100

    day 5: part one

--- 05/main.go
+++ 05/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -16,16 +18,49 @@ func dbg(fmt string, v ...interface{}) {
 	}
 }
 
+func translate(s string) int {
+	b := strings.Map(func(r rune) rune {
+		switch r {
+		case 'F', 'L':
+			return '0'
+		case 'B', 'R':
+			return '1'
+		}
+		return -1
+	}, s)
+
+	dbg("b = %v", b)
+
+	i, err := strconv.ParseInt(b, 2, 8)
+	if err != nil {
+		panic("malformed input")
+	}
+
+	return int(i)
+}
+
+func getSeatID(s string) int {
+	r := translate(s[:7])
+	c := translate(s[7:])
+	dbg("Row: %v, Col: %v", r, c)
+
+	return r*8 + c
+}
+
 func main() {
 
 	s := bufio.NewScanner(os.Stdin)
+	max := 0
 	for s.Scan() {
 		l := s.Text()
+		r := getSeatID(l)
+		if r > max {
+			max = r
+		}
 
-		dbg("Line: %v\n", l)
 	}
 
-	log.Printf("Part 1: \n")
+	log.Printf("Part 1: %v\n", max)
 	log.Printf("Part 2: \n")
 
 }
