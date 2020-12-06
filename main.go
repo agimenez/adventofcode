77af26c9c61bcfa5a64e5a8b02abc7bcf4099f48
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-06 12:49:49 +0100

    update template

--- main.go
+++ main.go
@@ -19,13 +19,14 @@ func dbg(fmt string, v ...interface{}) {
 func main() {
 
 	s := bufio.NewScanner(os.Stdin)
+	part1, part2 := 0, 0
 	for s.Scan() {
 		l := s.Text()
 
 		dbg("Line: %v\n", l)
 	}
 
-	log.Printf("Part 1: \n")
-	log.Printf("Part 2: \n")
+	log.Printf("Part 1: %v\n", part1)
+	log.Printf("Part 2: %v\n", part2)
 
 }
