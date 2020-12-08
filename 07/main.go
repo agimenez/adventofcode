8af4945ad4e7b1294eba145216c4125801665a31
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-08 19:41:35 +0100

    day 7: parsing input

--- 07/main.go
+++ 07/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-	debug = false
+	debug = true
 )
 
 func dbg(fmt string, v ...interface{}) {
@@ -20,10 +22,36 @@ func main() {
 
 	s := bufio.NewScanner(os.Stdin)
 	part1, part2 := 0, 0
+	contains := make(map[string]map[string]int)
+	containedBy := make(map[string][]string)
 	for s.Scan() {
 		l := s.Text()
 
-		dbg("Line: %v\n", l)
+		s1 := strings.Split(l, " bags contain ")
+		if s1[1] == "no other bags." {
+			continue
+		}
+
+		container := s1[0]
+		contains[container] = make(map[string]int)
+		contents := strings.Split(s1[1], ", ")
+		dbg("Container: %v", container)
+		for _, bag := range contents {
+			words := strings.Split(bag, " ")
+			num, err := strconv.Atoi(words[0])
+			if err != nil {
+				panic("can't parse input!")
+			}
+
+			name := words[1] + " " + words[2]
+			contains[container][name] = num
+			containedBy[name] = append(containedBy[name], container)
+
+		}
+
+		dbg("Contains: %+v", contains)
+		dbg("ContainedBy: %+v", containedBy)
+
 	}
 
 	log.Printf("Part 1: %v\n", part1)
