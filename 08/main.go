a2bc03089cef9e08a7691cee9150a32ef06a81f0
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-08 22:49:48 +0100

    day 8: part one

--- 08/main.go
+++ 08/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"bufio"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -16,15 +18,68 @@ func dbg(fmt string, v ...interface{}) {
 	}
 }
 
+type Machine struct {
+	mem []string
+	pc  int
+	acc int
+}
+
+func NewMachine(program string) *Machine {
+	dbg("prog: %v", program)
+	return &Machine{
+		pc:  0,
+		acc: 0,
+		mem: strings.Split(program, "\n"),
+	}
+
+}
+
+func (m *Machine) Step() {
+	fetch := m.mem[m.pc]
+	instr := strings.Split(fetch, " ")
+	op := instr[0]
+	arg, err := strconv.Atoi(instr[1])
+	if err != nil {
+		panic("error parsing argument")
+	}
+
+	addr := m.pc
+	switch op {
+	case "jmp":
+		addr += arg
+	case "acc":
+		m.acc += arg
+		addr++
+	case "nop":
+		addr++
+	}
+
+	m.pc = addr
+}
+
+func runUntilLoop(m *Machine) {
+	seen := map[int]bool{}
+	for {
+		if _, ok := seen[m.pc]; ok {
+			return
+		}
+		seen[m.pc] = true
+		m.Step()
+	}
+}
+
 func main() {
 
-	s := bufio.NewScanner(os.Stdin)
 	part1, part2 := 0, 0
-	for s.Scan() {
-		l := s.Text()
-
-		dbg("Line: %v\n", l)
+	p, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
 	}
+
+	m := NewMachine(string(p))
+	dbg("%#v", m)
+	runUntilLoop(m)
+	part1 = m.acc
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
