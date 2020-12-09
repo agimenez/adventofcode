5a6a78547162ff14438b217db2fdf78baf9e7d9f
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-09 17:57:02 +0100

    day 7: fix off-by-one error. Of course there was an off-by-one error.

--- 07/main.go
+++ 07/main.go
@@ -39,7 +39,7 @@ func findContains(contains map[string]map[string]int, s string) int {
 	dbg("[%v]%sFinding %v", depth, spc, s)
 	if len(contains[s]) == 0 {
 		dbg("[%v]%s -> no containers!", depth, spc)
-		return 1
+		return 0
 	}
 
 	acc := 0
@@ -49,11 +49,11 @@ func findContains(contains map[string]map[string]int, s string) int {
 		c := findContains(contains, bag)
 		dbg("[%v]%s'%v' contains a total of %v bags, times %v", depth, spc, bag, c, count)
 
-		acc += count * findContains(contains, bag)
+		acc += count + count*findContains(contains, bag)
 	}
 
 	depth--
-	return acc + 1
+	return acc
 }
 
 func deduplicate(list []string) []string {
@@ -111,9 +111,7 @@ func main() {
 	bags := deduplicate(findContainers(containedBy, "shiny gold"))
 	part1 = len(bags)
 
-	// The recursive function will account for the shiny gold bag itself, and we are
-	// only asked the number of bags contained in it
-	part2 = findContains(contains, "shiny gold") - 1
+	part2 = findContains(contains, "shiny gold")
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
