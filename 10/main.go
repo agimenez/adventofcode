6fef650ac4aab9de5f7b5e1b148cc7f44bf9cc37
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-11 19:47:35 +0100

    day 10: part two

--- 10/main.go
+++ 10/main.go
@@ -37,11 +37,32 @@ func getVDiff(nums []int) int {
 	return (diffs[3] + 1) * diffs[1]
 }
 
+// This is shameless stolen from Reddit. Thanks, kaur_virunurm
+// It seems you can use memoization, recursivity, and something called "Tribonacci",
+// but I can't be bothered.
+// https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfbo61q/?utm_source=reddit&utm_medium=web2x&context=3
+func getCombinations(nums []int) int {
+	nums = append([]int{0}, nums...)
+
+	c := make(map[int]int)
+	c[0] = 1
+
+	for _, n := range nums {
+		dbg("n: %v", n)
+		c[n+1] += c[n]
+		c[n+2] += c[n]
+		c[n+3] += c[n]
+		dbg("%#v", c)
+	}
+
+	return c[nums[len(nums)-1]+3]
+}
+
 func main() {
 
 	s := bufio.NewScanner(os.Stdin)
 	part1, part2 := 0, 0
-	nums := make([]int, 100)
+	nums := make([]int, 0, 100)
 	for s.Scan() {
 		l := s.Text()
 		n, err := strconv.Atoi(l)
@@ -55,6 +76,8 @@ func main() {
 
 	sort.Ints(nums)
 	part1 = getVDiff(nums)
+
+	part2 = getCombinations(nums)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
