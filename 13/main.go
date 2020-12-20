91ea16a77cd4630d13c4ee87b8b344c058f31bc2
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-20 19:25:51 +0100

    day 13: part one

--- 13/main.go
+++ 13/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -21,15 +23,49 @@ func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 	flag.Parse()
 }
+
+func findEarliest(ts int, buses []int) (int, int) {
+	cur := ts
+	for {
+		for i := range buses {
+			if cur%buses[i] == 0 {
+				return cur, buses[i]
+			}
+		}
+
+		cur++
+	}
+}
 func main() {
 
-	s := bufio.NewScanner(os.Stdin)
 	part1, part2 := 0, 0
-	for s.Scan() {
-		l := s.Text()
-
-		dbg("Line: %v\n", l)
+	p, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
 	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+
+	ts, err := strconv.Atoi(lines[0])
+	if err != nil {
+		panic("could not parse timestamp")
+	}
+
+	buses := []int{}
+	times := strings.Split(lines[1], ",")
+	for _, l := range times {
+		if l == "x" {
+			continue
+		}
+
+		b, err := strconv.Atoi(l)
+		if err != nil {
+			panic("could not parse busID")
+		}
+		buses = append(buses, b)
+	}
+	t, b := findEarliest(ts, buses)
+	part1 = (t - ts) * b
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
