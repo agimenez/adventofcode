c0e711ae9ddecea5b1294b2c3561cbd0f626180a
L. Alberto Giménez <agimenez@sysvalve.es>
2020-12-31 18:37:17 +0100

    day 13: part two

--- 13/main.go
+++ 13/main.go
@@ -36,6 +36,31 @@ func findEarliest(ts int, buses map[int]int) (int, int) {
 		cur++
 	}
 }
+
+// This seems to be solves using Chinese Remainder Theorem, but the awesome simple
+// solution from lizthegrey is more the "intuitive" form I had in mind, but couldn't
+// quite get. The following code is copied (and slightly modified to make it more
+// meaningful for me) from https://github.com/lizthegrey/adventofcode/blob/main/2020/day13.go
+// What this does is to define timestamp steps for the search (tsStep), which is
+// basically the LCM of the bus IDs (which are coprime).
+// We add the current found timestamp to the LCM of the previous buses until the
+// current one in a loop to search for a timestamp for which ts + bus offset is a
+// multiple of the bus ID, which means that the condition is satisfied
+func findEarliestAndSubsequent(buses map[int]int) int {
+	ts := 0
+	tsStep := 1
+
+	for bus, offset := range buses {
+		for (ts+offset)%bus != 0 {
+			ts += tsStep
+		}
+
+		tsStep *= bus
+	}
+
+	return ts
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -66,6 +91,8 @@ func main() {
 	}
 	t, b := findEarliest(ts, buses)
 	part1 = (t - ts) * b
+
+	part2 = findEarliestAndSubsequent(buses)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
