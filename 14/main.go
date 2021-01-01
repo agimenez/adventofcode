1d1c47555adb995adf8f769875f21532ee178713
L. Alberto Giménez <agimenez@sysvalve.es>
2021-01-01 17:18:06 +0100

    day 14: prepare changes and refactor for part two

--- 14/main.go
+++ 14/main.go
@@ -24,10 +24,14 @@ func init() {
 	flag.Parse()
 }
 
-type Masker map[int]int
+type Masker interface {
+	Apply(v uint64) interface{}
+}
 
-func NewMasker(mask string) Masker {
-	m := make(map[int]int)
+type ValueMask map[int]int
+
+func NewValueMasker(mask string) ValueMask {
+	m := make(ValueMask)
 	trans := map[rune]int{
 		'1': 1,
 		'0': 0,
@@ -45,7 +49,7 @@ func NewMasker(mask string) Masker {
 	return m
 }
 
-func (m Masker) Apply(v uint64) uint64 {
+func (m ValueMask) Apply(v uint64) interface{} {
 	dbg("vi = %036b", v)
 	for pos, bit := range m {
 		dbg("  pos %v, bit %v", pos, bit)
@@ -61,20 +65,37 @@ func (m Masker) Apply(v uint64) uint64 {
 	return v
 }
 
+type AddressMask struct {
+	mask     string
+	floating int
+}
+
+func NewAddressMasker(mask string) AddressMask {
+	m := AddressMask{
+		mask:     mask,
+		floating: 0,
+	}
+
+	return m
+}
+
+func (m AddressMask) Apply(a uint64) interface{} {
+	return a
+}
+
 type Computer struct {
 	mem  map[int]uint64
 	mask Masker
 }
 
-func NewComputer() *Computer {
+func NewComputer(m Masker) *Computer {
 	return &Computer{
 		mem:  make(map[int]uint64),
-		mask: NewMasker(""),
+		mask: m,
 	}
 }
 
 func (c *Computer) Write(a int, v uint64) {
-	v = c.mask.Apply(v)
 	c.mem[a] = v
 	dbg("mem[%d] = %036b", a, v)
 }
@@ -88,13 +109,38 @@ func (c *Computer) SumMem() uint64 {
 
 	return res
 }
-func (c *Computer) Run(lines []string) {
+func (c *Computer) RunMaskValues(lines []string) {
 	for i := range lines {
 		l := strings.Split(lines[i], " = ")
 		op, v := l[0], l[1]
 		switch op[:4] {
 		case "mask":
-			c.mask = NewMasker(v)
+			c.mask = NewValueMasker(v)
+		case "mem[":
+			addr, err := strconv.Atoi(op[4 : len(op)-1])
+			if err != nil {
+				panic("Can't parse address")
+			}
+
+			val, err := strconv.Atoi(v)
+			if err != nil {
+				panic("Can't parse value")
+			}
+
+			v64 := c.mask.Apply(uint64(val)).(uint64)
+			c.Write(addr, v64)
+		}
+	}
+
+}
+
+func (c *Computer) RunMaskAddresses(lines []string) {
+	for i := range lines {
+		l := strings.Split(lines[i], " = ")
+		op, v := l[0], l[1]
+		switch op[:4] {
+		case "mask":
+			c.mask = NewAddressMasker(v)
 		case "mem[":
 			addr, err := strconv.Atoi(op[4 : len(op)-1])
 			if err != nil {
@@ -109,7 +155,6 @@ func (c *Computer) Run(lines []string) {
 			c.Write(addr, uint64(val))
 		}
 	}
-
 }
 
 func main() {
@@ -121,12 +166,12 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	maskString := strings.Split(lines[0], " ")[2]
-	dbg("Mask: %#v", maskString)
-	c := NewComputer()
-	c.Run(lines)
-	dbg("computer: %#v", c)
+	c := NewComputer(NewValueMasker(""))
+	c.RunMaskValues(lines)
 	part1 = c.SumMem()
+
+	c = NewComputer(NewAddressMasker(""))
+	c.RunMaskAddresses(lines)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
