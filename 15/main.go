ec88456654ebc9d988e119fe08294b1b73d3c727
L. Alberto Giménez <agimenez@sysvalve.es>
2021-01-03 14:27:27 +0100

    day 15: init

---
+++ 15/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	debug bool
+)
+
+func dbg(fmt string, v ...interface{}) {
+	if debug {
+		log.Printf(fmt, v...)
+	}
+}
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+	flag.Parse()
+}
+
+func main() {
+
+	part1, part2 := 0, 0
+	p, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
+	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+
+	numbers := strings.Split(lines[0], ",")
+	dbg("%v", numbers)
+
+	log.Printf("Part 1: %v\n", part1)
+	log.Printf("Part 2: %v\n", part2)
+
+}
