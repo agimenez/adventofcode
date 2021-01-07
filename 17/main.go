954360843a31190aa084ab93131edadc10ca206b
L. Alberto Giménez <agimenez@sysvalve.es>
2021-01-07 15:57:13 +0100

    day 17: part two

--- 17/main.go
+++ 17/main.go
@@ -24,7 +24,7 @@ func init() {
 }
 
 type Cube struct {
-	x, y, z int
+	x, y, z, w int
 }
 
 func (c Cube) Add(c2 Cube) Cube {
@@ -32,10 +32,11 @@ func (c Cube) Add(c2 Cube) Cube {
 		c.x + c2.x,
 		c.y + c2.y,
 		c.z + c2.z,
+		c.w + c2.w,
 	}
 }
 
-func (c Cube) Neighbours() []Cube {
+func (c Cube) Neighbours3() []Cube {
 	n := []Cube{}
 	for x := -1; x <= 1; x++ {
 		for y := -1; y <= 1; y++ {
@@ -44,7 +45,7 @@ func (c Cube) Neighbours() []Cube {
 					continue
 				}
 
-				n = append(n, c.Add(Cube{x, y, z}))
+				n = append(n, c.Add(Cube{x, y, z, 0}))
 			}
 		}
 	}
@@ -52,10 +53,31 @@ func (c Cube) Neighbours() []Cube {
 	return n
 }
 
-func (c Cube) ActiveNeighbours(active map[Cube]bool) int {
+func (c Cube) Neighbours4() []Cube {
+	n := []Cube{}
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				for w := -1; w <= 1; w++ {
+					if x == 0 && y == 0 && z == 0 && w == 0 {
+						continue
+					}
+
+					n = append(n, c.Add(Cube{x, y, z, w}))
+				}
+			}
+		}
+	}
+
+	return n
+}
+
+type NeighboursFunc func() []Cube
+
+func (c Cube) ActiveNeighbours(active map[Cube]bool, neighbours NeighboursFunc) int {
 	count := 0
 
-	for _, n := range c.Neighbours() {
+	for _, n := range neighbours() {
 		if active[n] {
 			count++
 		}
@@ -64,7 +86,7 @@ func (c Cube) ActiveNeighbours(active map[Cube]bool) int {
 	return count
 }
 
-func mapCopy(src map[Cube]bool) map[Cube]bool {
+func SpaceCopy(src map[Cube]bool) map[Cube]bool {
 	dst := map[Cube]bool{}
 	for k, v := range src {
 		dst[k] = v
@@ -99,15 +121,17 @@ func main() {
 	for y := range lines {
 		for x, c := range lines[y] {
 			if c == '#' {
-				active[Cube{x, y, 0}] = true
+				active[Cube{x, y, 0, 0}] = true
 			}
 		}
 	}
+	active4 := SpaceCopy(active)
 
 	for i := 0; i < 6; i++ {
-		prev := mapCopy(active)
+		// 3 dims
+		prev := SpaceCopy(active)
 		for c := range active {
-			for _, n := range c.Neighbours() {
+			for _, n := range c.Neighbours3() {
 				// make them explicitly present in the map to calculate their
 				// possible values
 				prev[n] = prev[n]
@@ -115,7 +139,7 @@ func main() {
 		}
 
 		for cube, act := range prev {
-			n := cube.ActiveNeighbours(prev)
+			n := cube.ActiveNeighbours(prev, cube.Neighbours3)
 			if act && (n != 2 && n != 3) {
 				delete(active, cube)
 			} else if n == 3 {
@@ -123,10 +147,31 @@ func main() {
 			}
 		}
 
+		// 4 dims
+		prev4 := SpaceCopy(active4)
+		for c := range active4 {
+			for _, n := range c.Neighbours4() {
+				// make them explicitly present in the map to calculate their
+				// possible values
+				prev4[n] = prev4[n]
+			}
+		}
+
+		for cube, act := range prev4 {
+			n := cube.ActiveNeighbours(prev4, cube.Neighbours4)
+			if act && (n != 2 && n != 3) {
+				delete(active4, cube)
+			} else if n == 3 {
+				active4[cube] = true
+			}
+		}
+
 	}
 
 	part1 = CountActive(active)
 	log.Printf("Part 1: %v\n", part1)
+
+	part2 = CountActive(active4)
 	log.Printf("Part 2: %v\n", part2)
 
 }
