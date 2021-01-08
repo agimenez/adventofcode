4e92454daf5c290036f5c95f732509dcd8501c9b
L. Alberto Giménez <agimenez@sysvalve.es>
2021-01-08 21:06:57 +0100

    day 18: working on alternative solution for part 2

--- 18/main.go
+++ 18/main.go
@@ -41,7 +41,7 @@ func Mul(a, b int) int {
 	return a * b
 }
 
-var depth int
+var depth int = -1
 
 func Solve(tokens []string) (int, []string) {
 	acc := 0
@@ -83,12 +83,139 @@ func Solve(tokens []string) (int, []string) {
 	return acc, []string{}
 }
 
-func SolveExpression(e string) int {
+type Stackable interface {
+	Value() int
+	Str() string
+}
+
+type Stack []Stackable
+
+func (s Stack) Push(e Stackable) Stack {
+	return append(s, e)
+}
+
+func (s Stack) Pop() (Stackable, Stack) {
+	return s[len(s)-1], s[:len(s)-1]
+}
+
+func (s Stack) Empty() bool {
+	return len(s) == 0
+}
+
+func (s Stack) Top() Stackable {
+	return s[len(s)-1]
+}
+
+type Token string
+
+func (t Token) Value() int {
+	return 0
+}
+
+func (t Token) Str() string {
+	return string(t)
+}
+
+type Number int
+
+func NewNumber(s string) Number {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("Cannot parse '%s", s)
+	}
+
+	return Number(n)
+}
+func (n Number) Value() int {
+	return int(n)
+}
+func (n Number) Str() string {
+	return fmt.Sprintf("%d", n)
+}
+
+type OpsMap map[string]struct {
+	Precedence int
+	Eval       func(a, b int) int
+}
+
+// Shunting-yard algorithm (https://en.wikipedia.org/wiki/Shunting-yard_algorithm)
+func Solve2(tokens []string) (int, []string) {
+	ops := Stack{}
+	res := Stack{}
+	opMap := OpsMap{
+		"+": {1, Sum},
+		"*": {2, Mul},
+	}
+	for _, tok := range tokens {
+		switch tok {
+		case "(":
+			ops = ops.Push(Token(tok))
+		case ")":
+			top := ops.Top().Str()
+			for top != "(" {
+
+				var op, op1, op2 Stackable
+				op, ops = ops.Pop()
+				op1, res = res.Pop()
+				op2, res = res.Pop()
+
+				r := opMap[op.Str()].Eval(op1.Value(), op2.Value())
+				res = res.Push(Number(r))
+
+				top = ops.Top().Str()
+			}
+		case "+", "*":
+			if !ops.Empty() {
+				top := ops.Top().Str()
+				for _, isOp := opMap[top]; isOp && opMap[top].Precedence >= opMap[tok].Precedence; {
+					var op, op1, op2 Stackable
+
+					op, ops = ops.Pop()
+					op1, res = res.Pop()
+					op2, res = res.Pop()
+
+					r := opMap[op.Str()].Eval(op1.Value(), op2.Value())
+					res = res.Push(Number(r))
+
+					if ops.Empty() {
+						break
+					}
+
+					top = ops.Top().Str()
+				}
+			}
+
+			ops = ops.Push(Token(tok))
+
+		default:
+			res = res.Push(NewNumber(tok))
+		}
+
+	}
+
+	for len(ops) > 0 {
+		var op, op1, op2 Stackable
+
+		op, ops = ops.Pop()
+		op1, res = res.Pop()
+		op2, res = res.Pop()
+
+		r := opMap[op.Str()].Eval(op1.Value(), op2.Value())
+		res = res.Push(Number(r))
+	}
+	dbg("Ops: %v", ops)
+	dbg("Res: %v", res)
+
+	return res[0].Value(), []string{}
+}
+
+func SolveExpression(e string, solve func([]string) (int, []string)) int {
 	e = strings.ReplaceAll(e, "(", "( ")
 	e = strings.ReplaceAll(e, ")", " )")
 	tokens := strings.Split(e, " ")
 
-	res, _ := Solve(tokens)
+	res, _ := solve(tokens)
+	fmt.Printf("'%s' = %d\n", e, res)
 	return res
 }
 
@@ -98,7 +225,9 @@ func main() {
 	s := bufio.NewScanner(os.Stdin)
 
 	for s.Scan() {
-		part1 += SolveExpression(s.Text())
+		//part1 += SolveExpression(s.Text(), Solve)
+		p2 := SolveExpression(s.Text(), Solve2)
+		part2 += p2
 	}
 
 	log.Printf("Part 1: %v\n", part1)
--- 18/main_test.go
+++ 18/main_test.go
@@ -2,23 +2,31 @@ package main
 
 import "testing"
 
-func TestGame(t *testing.T) {
+func TestSolve(t *testing.T) {
 	tests := []struct {
-		input    string
-		expected int
+		input     string
+		expected  int
+		expected2 int
 	}{
-		{"1 + 2 * 3 + 4 * 5 + 6", 71},
-		{"1 + (2 * 3) + (4 * (5 + 6))", 51},
-		{"2 * 3 + (4 * 5)", 26},
-		{"5 + (8 * 3 + 9 + 3 * 4 * 3)", 437},
-		{"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240},
-		{"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632},
+		{"1 + 2 * 3 + 4 * 5 + 6", 71, 231},
+		{"1 + (2 * 3) + (4 * (5 + 6))", 51, 51},
+		{"2 * 3 + (4 * 5)", 26, 46},
+		{"5 + (8 * 3 + 9 + 3 * 4 * 3)", 437, 1445},
+		{"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240, 669060},
+		{"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632, 23340},
+		{"(2 * 5) * 2 * (4 + 7 + 2) + 7", 13632, 400},
+		{"(7 + 6 + (8 + 3) + 5 + 8 * 2) * 3", 13632, 222},
 	}
 
 	for _, tt := range tests {
-		v := SolveExpression(tt.input)
+		v := SolveExpression(tt.input, Solve)
 		if v != tt.expected {
-			t.Errorf("Got %d, expected %d", v, tt.expected)
+			t.Errorf("Got %d, expected %d Solve '%s'", v, tt.expected, tt.input)
+		}
+
+		v = SolveExpression(tt.input, Solve2)
+		if v != tt.expected2 {
+			t.Errorf("Got %d, expected %d Solve2 '%s'", v, tt.expected2, tt.input)
 		}
 	}
 }
