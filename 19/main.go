7418841c703e4fb93240560aff79dfb42fcfad0f
L. Alberto Giménez <agimenez@sysvalve.es>
2021-01-10 19:05:14 +0100

    day 19: part one

--- 19/main.go
+++ 19/main.go
@@ -23,6 +23,91 @@ func init() {
 	flag.Parse()
 }
 
+type Validator interface {
+	Eval(string) (bool, string)
+}
+
+type Rule struct {
+	set      *Matcher
+	subrules [][]string
+}
+
+func NewRule(s string, m *Matcher) Rule {
+	sets := strings.Split(s, " | ")
+	r := Rule{
+		set:      m,
+		subrules: make([][]string, len(sets)),
+	}
+	for i := range sets {
+		r.subrules[i] = []string{}
+		for _, v := range strings.Split(sets[i], " ") {
+			r.subrules[i] = append(r.subrules[i], v)
+		}
+
+	}
+
+	return r
+}
+func (r Rule) Eval(in string) (bool, string) {
+	if len(in) == 0 {
+		return false, ""
+	}
+
+	for set := range r.subrules {
+		rem := in
+		match := true
+		for _, rule := range r.subrules[set] {
+			var ok bool
+			if ok, rem = (*r.set)[rule].Eval(rem); !ok {
+				match = false
+				break
+			}
+
+		}
+
+		if match {
+			return true, rem
+		}
+	}
+
+	return false, in
+}
+
+type Literal rune
+
+func NewLiteral(s string) Literal {
+	return Literal(s[1])
+}
+
+func (l Literal) Eval(in string) (bool, string) {
+	if len(in) > 0 && in[0] == byte(l) {
+		return true, in[1:]
+	}
+
+	return false, in
+}
+
+func NewValidator(s string, m *Matcher) Validator {
+	if s[0] == '"' {
+		return NewLiteral(s)
+	}
+
+	return NewRule(s, m)
+}
+
+type Matcher map[string]Validator
+
+func (m Matcher) Match(in string) bool {
+	ok, rem := m["0"].Eval(in)
+
+	return ok && len(rem) == 0
+}
+
+func (m *Matcher) AddRule(s string) {
+	parts := strings.Split(s, ": ")
+	(*m)[parts[0]] = NewValidator(parts[1], m)
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -32,20 +117,24 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %+v", lines)
 
+	m := Matcher{}
 	l := 0
 	for ; lines[l] != ""; l++ {
-		parts := strings.Split(lines[l], ": ")
-		//rules[parts[0]] = NewValidator(parts[1])
-		dbg("%+v", parts)
+		m.AddRule(lines[l])
 	}
+
+	dbg("rules:%+v", &m)
 
 	l++
 
-	//jump my ticket + newline + "nearby tickets"
 	for _, l := range lines[l:] {
-		dbg("%v", l)
+		if m.Match(l) {
+			part1++
+			dbg("%v matched!", l)
+		} else {
+			dbg("%v did not match", l)
+		}
 
 	}
 
