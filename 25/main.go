b475571e3a963b1925bd5d8f23ab8497991cc82f
L. Alberto Giménez <agimenez@sysvalve.es>
2021-02-09 19:50:45 +0100

    day 25: part one

--- 25/main.go
+++ 25/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -22,6 +23,40 @@ func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 	flag.Parse()
 }
+
+type Device struct {
+	PubKey   int
+	LoopSize int
+}
+
+func (d Device) DeriveLoopSize(subject int) Device {
+	d2 := d
+	val := 1
+	for {
+		val *= subject
+		val %= 20201227
+		d2.LoopSize++
+
+		if val == d2.PubKey {
+			break
+		}
+	}
+
+	return d2
+}
+
+func (d Device) EncryptionKey(d2 Device) int {
+
+	val := 1
+
+	for i := 0; i < d.LoopSize; i++ {
+		val *= d2.PubKey
+		val %= 20201227
+	}
+
+	return val
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -31,7 +66,26 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %#v", lines)
+
+	n, err := strconv.Atoi(lines[0])
+	if err != nil {
+		log.Fatalf("Could not parse %s: %v", lines[0], err)
+	}
+	card := Device{
+		PubKey: n,
+	}
+	card = card.DeriveLoopSize(7)
+
+	n, err = strconv.Atoi(lines[1])
+	if err != nil {
+		log.Fatalf("Could not parse %s: %v", lines[0], err)
+	}
+	door := Device{
+		PubKey: n,
+	}
+	door = door.DeriveLoopSize(7)
+
+	part1 = card.EncryptionKey(door)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
