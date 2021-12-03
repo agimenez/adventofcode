7c447380828ef5c66e40e2b812463d06a9e9f185
L. Alberto Giménez <agimenez@sysvalve.es>
2021-12-03 20:25:11 +0100

    2021-03: part two

--- 2021/03/main.go
+++ 2021/03/main.go
@@ -38,9 +38,63 @@ func gammaBalance(b []int) (int64, int64) {
 	}
 	gamma, _ := strconv.ParseInt(g, 2, 32)
 	epsilon, _ := strconv.ParseInt(e, 2, 32)
-	dbg("Balance for %v: gamma %0b rate %d, epsilon %0b, rate %d", b, gamma, gamma, epsilon, epsilon)
+	//dbg("Balance for %v: gamma %0b rate %d, epsilon %0b, rate %d", b, gamma, gamma, epsilon, epsilon)
 
 	return gamma, epsilon
+}
+
+const (
+	MostCommon = iota
+	LeastCommon
+)
+
+func recurseRating(in []string, pos int, criteria int) []string {
+	if len(in) <= 1 {
+		return in
+	}
+
+	split := map[byte][]string{}
+
+	balance := 0
+	dbg("=== Position %d ===", pos)
+	for _, v := range in {
+		split[v[pos]] = append(split[v[pos]], v)
+
+		if v[pos] == '0' {
+			balance--
+		} else {
+			balance++
+		}
+		dbg("%s [%d] -> %c: %d", v, pos, v[pos], balance)
+	}
+
+	var out []string
+	switch criteria {
+	case MostCommon:
+		if balance >= 0 {
+			out = split['1']
+		} else {
+			out = split['0']
+		}
+
+	case LeastCommon:
+		if balance >= 0 {
+			out = split['0']
+		} else {
+			out = split['1']
+		}
+	}
+	dbg("Kept: %v", out)
+
+	return recurseRating(out, pos+1, criteria)
+}
+
+func Rating(report []string, criteria int) int64 {
+
+	r := recurseRating(report, 0, criteria)
+
+	v, _ := strconv.ParseInt(r[0], 2, 32)
+	return v
 }
 
 func main() {
@@ -68,6 +122,11 @@ func main() {
 	}
 	gamma, epsilon := gammaBalance(balance)
 	part1 = int(gamma * epsilon)
+
+	oxygen := Rating(lines, MostCommon)
+	co2 := Rating(lines, LeastCommon)
+
+	part2 = int(oxygen * co2)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
