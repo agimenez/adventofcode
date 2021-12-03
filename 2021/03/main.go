357057faa8ea6c08392a579fdba043cfe6a3009a
L. Alberto Giménez <agimenez@sysvalve.es>
2021-12-03 12:04:43 +0100

    2021-03: part one (ugly as fsck)

--- 2021/03/main.go
+++ 2021/03/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -22,6 +23,26 @@ func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 	flag.Parse()
 }
+
+func gammaBalance(b []int) (int64, int64) {
+	var g string
+	var e string
+	for i := range b {
+		if b[i] >= 0 {
+			g += "1"
+			e += "0"
+		} else {
+			g += "0"
+			e += "1"
+		}
+	}
+	gamma, _ := strconv.ParseInt(g, 2, 32)
+	epsilon, _ := strconv.ParseInt(e, 2, 32)
+	dbg("Balance for %v: gamma %0b rate %d, epsilon %0b, rate %d", b, gamma, gamma, epsilon, epsilon)
+
+	return gamma, epsilon
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -32,6 +53,21 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	dbg("lines: %#v", lines)
+
+	balance := make([]int, len(lines[0]))
+	for i := range lines {
+		dbg("Line: %+v", lines[i])
+		for j := range lines[i] {
+			if lines[i][j] == '0' {
+				balance[j]--
+			} else {
+				balance[j]++
+			}
+		}
+		dbg("Balance: %v", balance)
+	}
+	gamma, epsilon := gammaBalance(balance)
+	part1 = int(gamma * epsilon)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
