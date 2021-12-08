20d0f90904ba460f0416b4a7332a6b1d28b7f986
L. Alberto Giménez <agimenez@sysvalve.es>
2021-12-09 00:03:07 +0100

    2021-08: part one

--- 2021/08/main.go
+++ 2021/08/main.go
@@ -22,6 +22,32 @@ func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 	flag.Parse()
 }
+
+func parseEntry(s string) ([]string, []string) {
+	parts := strings.Split(s, " | ")
+	signals := strings.Fields(parts[0])
+	outputs := strings.Fields(parts[1])
+
+	return signals, outputs
+}
+
+func solvePart1(in []string) int {
+	count := 0
+	for _, entry := range in {
+		_, outputValues := parseEntry(entry)
+		dbg("outputs: %v", outputValues)
+
+		for _, v := range outputValues {
+			switch len(v) {
+			case 2, 3, 4, 7:
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -31,7 +57,7 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %#v", lines)
+	part1 = solvePart1(lines)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
