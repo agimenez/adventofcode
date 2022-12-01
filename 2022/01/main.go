694985f3ab5f118ac7d125c7351d7d7f83845f35
L. Alberto Giménez <agimenez@sysvalve.es>
2022-12-01 10:11:25 +0100

    2022-01: part one

--- 2022/01/main.go
+++ 2022/01/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -22,6 +23,28 @@ func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 	flag.Parse()
 }
+
+func maxCals(in []string) int {
+	elfSum := 0
+	maxSum := 0
+
+	for _, c := range in {
+		if c == "" {
+			if elfSum > maxSum {
+				maxSum = elfSum
+			}
+			elfSum = 0
+			continue
+		}
+
+		cals, _ := strconv.Atoi(c)
+		elfSum += cals
+
+	}
+
+	return maxSum
+}
+
 func main() {
 
 	part1, part2 := 0, 0
@@ -32,6 +55,8 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	dbg("lines: %#v", lines)
+
+	part1 = maxCals(lines)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
