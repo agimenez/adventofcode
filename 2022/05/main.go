6ac36764d86089d0abb0702ca66420a10f18fc16
L. Alberto Giménez <agimenez@sysvalve.es>
2022-12-28 22:39:34 +0100

    2022-05: init

---
+++ 2022/05/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	debug bool
+)
+
+func dbg(fmt string, v ...interface{}) {
+	if debug {
+		log.Printf(fmt, v...)
+	}
+}
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+}
+func main() {
+	flag.Parse()
+
+	part1, part2 := 0, 0
+	p, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
+	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+	dbg("lines: %#v", lines)
+
+	log.Printf("Part 1: %v\n", part1)
+	log.Printf("Part 2: %v\n", part2)
+
+}
