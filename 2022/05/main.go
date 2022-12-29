871cfed256930e24665d9c51b019c94df62145bb
L. Alberto Giménez <agimenez@sysvalve.es>
2022-12-29 21:21:15 +0100

    2022-05: part one

--- 2022/05/main.go
+++ 2022/05/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -48,10 +50,43 @@ func (s Stack) Insert(b byte) Stack {
 	return s
 }
 
+type Crane []Stack
+
+func (c Crane) InsertCrate(stack int, crane byte) Crane {
+	if len(c) <= stack {
+		c2 := make(Crane, stack+1)
+		copy(c2, c)
+		c = c2
+	}
+
+	c[stack] = c[stack].Insert(crane)
+
+	return c
+}
+
+func (c Crane) MoveCrates(count, from, to int) Crane {
+	for ; count > 0; count-- {
+		s, crate := c[from].Pop()
+		c[from] = s
+		c[to] = c[to].Push(crate)
+	}
+
+	return c
+}
+
+func (c Crane) TopCrates() string {
+	var b bytes.Buffer
+	for _, s := range c {
+		b.WriteByte(s.Top())
+	}
+
+	return b.String()
+}
+
 func main() {
 	flag.Parse()
 
-	part1, part2 := 0, 0
+	part1, part2 := "", ""
 	p, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic("could not read input")
@@ -59,6 +94,28 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	dbg("lines: %#v", lines)
+
+	crane := Crane{}
+	dbg("crane: %#v", crane)
+	for _, line := range lines {
+		dbg("line: %v", line)
+		if strings.Contains(line, "[") {
+			for i := 1; i < len(line); i += 4 {
+				if line[i] != ' ' {
+					crane = crane.InsertCrate(i/4, line[i])
+				}
+			}
+			dbg("Crane: %q", crane)
+		}
+
+		if strings.Contains(line, "move") {
+			var count, src, dst int
+			fmt.Sscanf(line, "move %d from %d to %d", &count, &src, &dst)
+			crane = crane.MoveCrates(count, src-1, dst-1)
+			dbg("Crane: %q", crane)
+		}
+	}
+	part1 = crane.TopCrates()
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
