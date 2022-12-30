923bdab392500bdd9cf17b64832d545f8c04decf
L. Alberto Giménez <agimenez@sysvalve.es>
2022-12-30 11:00:34 +0100

    newday: automate downloading readme and input

--- newday.sh
+++ newday.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+test -z ${AOC_SESSION} && {
+	echo "Need to set AOC_SESSION environment variable"
+	exit 1
+}
+
 dir=$1
+year=${dir%%/*}
+day=$(printf "%d" ${dir##*/})
+
 mkdir -p ${dir}
 cp main.go main_test.go ${dir}
 cd ${dir}
-touch README.md input.txt test.txt
 
-echo "cd ${dir}"
+curl https://adventofcode.com/${year}/day/${day}/input \
+	--silent --cookie "session=${AOC_SESSION}" \
+	--output input.txt
+
+curl https://adventofcode.com/${year}/day/${day} \
+	--silent --cookie "session=${AOC_SESSION}" \
+	| html2markdown --mark-code --asterisk-emphasis > README.md
+
+touch test.txt
