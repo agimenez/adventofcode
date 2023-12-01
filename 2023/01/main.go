10214ced63675f12f89ac2c4756535526bf94cb7
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-01 16:47:57 +0100

    2023-01: part one

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 var (
@@ -31,7 +33,29 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %#v", lines)
+	//dbg("lines: %#v", lines)
+
+	for l := range lines {
+		first, last := -1, -1
+		for _, r := range lines[l] {
+			if !unicode.IsDigit(r) {
+				continue
+			}
+
+			v, _ := strconv.Atoi(string(r))
+
+			// Initial case: no first or last
+			if first == -1 {
+				first = v
+			}
+
+			// if it's a digit, will always be last
+			last = v
+		}
+
+		dbg("l: %s, %d%d", lines[l], first, last)
+		part1 += 10*first + last
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
