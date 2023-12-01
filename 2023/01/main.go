d78c632c98e38d5ac2eb4c499b36ca6eb099c1b6
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-01 17:59:54 +0100

    2021-01: part two

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -23,6 +23,20 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+var words = map[string]int{
+	"zero":  0,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
 func main() {
 	flag.Parse()
 
@@ -37,20 +51,34 @@ func main() {
 
 	for l := range lines {
 		first, last := -1, -1
-		for _, r := range lines[l] {
-			if !unicode.IsDigit(r) {
-				continue
+		dbg("l: %s", lines[l])
+		for i, r := range lines[l] {
+			if unicode.IsDigit(r) {
+				dbg("digit: %c", r)
+
+				v, _ := strconv.Atoi(string(r))
+
+				// Initial case: no first or last
+				if first == -1 {
+					first = v
+				}
+
+				// if it's a digit, will always be last
+				last = v
+			} else { // handle part 2
+				dbg("word: %s", lines[l][i:])
+				for w, d := range words {
+					if strings.HasPrefix(lines[l][i:], w) {
+						if first == -1 {
+							first = d
+						}
+
+						last = d
+					}
+				}
+
 			}
-
-			v, _ := strconv.Atoi(string(r))
-
-			// Initial case: no first or last
-			if first == -1 {
-				first = v
-			}
-
-			// if it's a digit, will always be last
-			last = v
+			dbg("first: %v, last: %v", first, last)
 		}
 
 		dbg("l: %s, %d%d", lines[l], first, last)
