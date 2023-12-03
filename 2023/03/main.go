88d8f0ad47efb101fa0e43a5d7b0de393ba99566
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-03 20:47:10 +0100

    2023-03: part one

--- 2023/03/main.go
+++ 2023/03/main.go
@@ -5,7 +5,11 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
+
+	"github.com/agimenez/adventofcode/utils"
 )
 
 var (
@@ -21,6 +25,35 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+type symbol struct {
+	coord utils.Point
+	val   rune
+}
+
+func isSymbol(c rune) bool {
+	return !(unicode.IsDigit(c) || c == '.')
+}
+
+func adjSymbol(l []string, x int, y int) bool {
+
+	//dbg("==== Check adjacent of {%v, %v} = %c", x, y, l[y][x])
+	for dx := x - 1; dx <= x+1; dx++ {
+		for dy := y - 1; dy <= y+1; dy++ {
+			if (dx == x && dy == y) || dx < 0 || dy < 0 || dx >= len(l[y]) || dy >= len(l) {
+				continue
+			}
+			//dbg("Checking {%v,%v} = %c", dx, dy, l[dy][dx])
+
+			if isSymbol(rune(l[dy][dx])) {
+				//dbg(" -> YES")
+				return true
+			}
+		}
+	}
+
+	return false
+}
 func main() {
 	flag.Parse()
 
@@ -31,7 +64,32 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %#v", lines)
+	//dbg("lines: %#v", lines)
+	for y, l := range lines {
+		partNo := ""
+		isPart := false
+		for x, c := range l {
+			if unicode.IsDigit(c) {
+				partNo += string(c)
+
+				if !isPart {
+					isPart = adjSymbol(lines, x, y)
+				}
+			}
+
+			// finished number, or end of line
+			if !unicode.IsDigit(c) || x >= len(l)-1 {
+				if isPart {
+					dbg("Adding %v,%v = %v", x, y, partNo)
+					v, _ := strconv.Atoi(partNo)
+					part1 += v
+				}
+				partNo = ""
+				isPart = false
+			}
+
+		}
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
