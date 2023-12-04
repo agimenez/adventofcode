71bc24b942d813e8f67b555ba3de181eb64dd365
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-04 11:44:18 +0100

    2023-04: part one

--- 2023/04/main.go
+++ 2023/04/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,6 +22,54 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+func listFromString(s string) []int {
+	l := []int{}
+
+	for _, num := range strings.Split(s, " ") {
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			continue
+		}
+
+		l = append(l, n)
+	}
+
+	return l
+}
+func mapList(s string) map[int]bool {
+	nums := listFromString(s)
+	m := map[int]bool{}
+	for _, n := range nums {
+		m[n] = true
+	}
+
+	return m
+}
+
+func getCardScore(card string) int {
+	numbers := strings.Split(card, ": ")[1]
+	parts := strings.Split(numbers, " | ")
+	winners := mapList(parts[0])
+	played := listFromString(parts[1])
+
+	wins := 0
+	dbg("winners: %v", winners)
+	for _, n := range played {
+		dbg("  -> Playing %v", n)
+		if _, ok := winners[n]; ok {
+			if wins == 0 {
+				wins = 1
+			} else {
+				wins *= 2
+			}
+
+			dbg("  -> WINNER! -> %v", wins)
+		}
+	}
+
+	return wins
+}
 func main() {
 	flag.Parse()
 
@@ -31,7 +80,12 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	dbg("lines: %#v", lines)
+	//dbg("lines: %#v", lines)
+	for _, card := range lines {
+		points := getCardScore(card)
+		part1 += points
+
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
