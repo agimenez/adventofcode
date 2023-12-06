779b6b7eee495be564194920ebeabed068b16a1f
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-06 12:54:30 +0100

    2023-06: part one

--- 2023/06/main.go
+++ 2023/06/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,10 +22,57 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+func atoi(s string) int {
+	n, _ := strconv.Atoi(s)
+
+	return n
+}
+
+type race struct {
+	time     int
+	distance int
+}
+
+type competition []race
+
+func newCompetition(s []string) competition {
+	c := competition{}
+
+	// Time
+	races := strings.Fields(strings.Split(s[0], ":")[1])
+	for _, r := range races {
+		c = append(c, race{time: atoi(r)})
+	}
+
+	// Distance
+	races = strings.Fields(strings.Split(s[1], ":")[1])
+	for i, r := range races {
+		c[i].distance = atoi(r)
+	}
+
+	return c
+}
+
+func (c competition) race(r int) []int {
+	race := c[r]
+	wins := []int{}
+
+	for time := 1; time < race.time; time++ {
+		dist := time * (race.time - time)
+		if dist > race.distance {
+			wins = append(wins, time)
+		}
+	}
+
+	return wins
+
+}
+
 func main() {
 	flag.Parse()
 
-	part1, part2 := 0, 0
+	part1, part2 := 1, 1
 	p, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic("could not read input")
@@ -32,6 +80,12 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	//dbg("lines: %#v", lines)
+	comp := newCompetition(lines)
+	dbg("%v", comp)
+	for i := range comp {
+		ways := comp.race(i)
+		part1 *= len(ways)
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
