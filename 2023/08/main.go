b071495a614d66b243d40ec2bcffda29f8a832d9
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-08 10:57:31 +0100

    2023-08: part one

--- 2023/08/main.go
+++ 2023/08/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -21,6 +22,40 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+type network map[string]map[rune]string
+
+func (n network) findNode(e string, ins []rune) []string {
+	path := []string{}
+	next := "AAA"
+
+	for i := 0; next != "ZZZ"; i = (i + 1) % len(ins) {
+		dbg("Trying  %c (%d)", ins[i], i)
+
+		path = append(path, next)
+		next = n[next][ins[i]]
+	}
+
+	return path
+}
+
+func parseMap(s []string) ([]rune, network) {
+	net := network{}
+
+	ins := []rune(s[0])
+	for _, l := range s[2:] {
+		var node, left, right string
+		fmt.Sscanf(l, "%s = (%3s, %3s)", &node, &left, &right)
+
+		net[node] = map[rune]string{
+			'L': left,
+			'R': right,
+		}
+
+	}
+
+	return ins, net
+}
 func main() {
 	flag.Parse()
 
@@ -32,6 +67,10 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	//dbg("lines: %#v", lines)
+	instructions, network := parseMap(lines)
+	dbg("ins: %s\nnetwork: %v", string(instructions), network)
+	path := network.findNode("ZZZ", instructions)
+	part1 = len(path)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
