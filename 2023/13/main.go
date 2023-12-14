2f876145a2f44ea754d76baea495a64dce33e434
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-14 16:41:37 +0100

    WIP: 2023-13: part one

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -21,6 +21,65 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
+func mirrorLine(s string) bool {
+	dbg(" -> mirrorLine %s", s)
+	for i := 0; i < len(s)/2; i++ {
+		dbg("    -> %c == %c", s[i], s[len(s)-i-1])
+		if s[i] != s[len(s)-i-1] {
+			dbg("    -> NAH!")
+			return false
+		}
+	}
+
+	dbg("    -> YUP!")
+	return true
+}
+
+func mirrorCol(s []string, startRow int, col int) bool {
+	for i := startRow; i < len(s); i++ {
+	}
+}
+func solve1(in []string) int {
+	res := 0
+
+	// First, check vertical reflection
+	for col := 0; col < len(in[0]); col++ {
+		dbg("Checking column %d", col)
+		mirror := true
+		for _, row := range in {
+			mirror = mirror && mirrorLine(row[col:])
+			if !mirror {
+				break
+			}
+		}
+
+		if mirror {
+			res += (len(in[0])-col)/2 + 1
+			break
+		}
+	}
+
+	// Check horizontal reflection
+	for row := 0; row < len(in); row++ {
+		dbg("Checking row %d", row)
+		mirror := true
+		for col := 0; col < len(in[0]); c++ {
+			mirror = mirror && mirrorCol(in, row, col)
+			if !mirror {
+				break
+			}
+		}
+
+		if mirror {
+			res += 100 * ((len(in)-row)/2 + 1)
+			break
+		}
+	}
+
+	return res
+}
+
 func main() {
 	flag.Parse()
 
@@ -32,6 +91,17 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	//dbg("lines: %#v", lines)
+	pattern := []string{}
+	for _, l := range lines {
+		if l == "" {
+			part1 += solve1(pattern)
+			pattern = []string{}
+			continue
+		}
+
+		pattern = append(pattern, l)
+
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
