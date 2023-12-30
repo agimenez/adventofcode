990bdde432954328925071c8f8549d19ff3eb8ca
L. Alberto Giménez <agimenez@sysvalve.es>
2023-12-30 21:56:49 +0100

    2019-12: part two

--- 2019/12/main.go
+++ 2019/12/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 )
 
@@ -128,14 +129,98 @@ func calculateEnergy(moons []Moon) int {
 	return total
 }
 
-func main() {
-	moons := getMoons(os.Stdin)
-
-	for i := 1; i <= 1000; i++ {
-		dbg(2, "step %d", i)
-		timeStep(moons)
+func extractField(m []Moon, sub string, field string) []int {
+	ret := []int{}
+	for i := range m {
+		switch sub + field {
+		case "Px":
+			ret = append(ret, m[i].Pos.x)
+		case "Py":
+			ret = append(ret, m[i].Pos.y)
+		case "Pz":
+			ret = append(ret, m[i].Pos.z)
+		case "Vx":
+			ret = append(ret, m[i].Vel.x)
+		case "Vy":
+			ret = append(ret, m[i].Vel.y)
+		case "Vz":
+			ret = append(ret, m[i].Vel.z)
+		}
 	}
 
-	energy := calculateEnergy(moons)
-	fmt.Printf("Energy: %d\n", energy)
+	return ret
+}
+func axisEquals(m1, m2 []Moon, axis string) bool {
+	s1 := extractField(m1, "P", axis)
+	s2 := extractField(m2, "P", axis)
+
+	s3 := extractField(m1, "V", axis)
+	s4 := extractField(m2, "V", axis)
+
+	return slices.Equal(s1, s2) && slices.Equal(s3, s4)
+}
+
+func main() {
+	part1 := 0
+	part2 := 0
+
+	moons := getMoons(os.Stdin)
+	origMoons := slices.Clone(moons)
+	axisCycles := map[string]int{}
+
+	for i := 1; ; i++ {
+		dbg(2, "step %d", i)
+		timeStep(moons)
+		if i == 1000 {
+			part1 = calculateEnergy(moons)
+		}
+
+		dbg(2, "Moons: %v", moons)
+		dbg(2, "Orig : %v", origMoons)
+
+		for _, axis := range []string{"x", "y", "z"} {
+			if _, ok := axisCycles[axis]; !ok && axisEquals(moons, origMoons, axis) {
+				axisCycles[axis] = i
+				dbg(1, "Found axis cycle (%q): %d", axis, i)
+				dbg(1, "Moons: %v", moons)
+				dbg(1, "Orig : %v", origMoons)
+
+			}
+		}
+		dbg(2, "Found cycles: %v", axisCycles)
+
+		if len(axisCycles) == 3 {
+			break
+		}
+	}
+
+	part2 = sliceLCM(func() []int {
+		ret := []int{}
+		for _, v := range axisCycles {
+			ret = append(ret, v)
+		}
+		return ret
+	}())
+
+	fmt.Printf("Energy: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func sliceLCM(numbers []int) int {
+	result := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		result = lcm(result, numbers[i])
+	}
+	return result
 }
