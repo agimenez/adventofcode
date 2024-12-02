0f794a9a6fbe4ea75d2b8580c9dab8055465d7ed
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-02 23:33:11 +0100

    2024-02: part one

--- 2024/02/main.go
+++ 2024/02/main.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/agimenez/adventofcode/utils"
 )
 
 var (
@@ -31,9 +34,51 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
+	for i := range lines {
+		if isSafe(lines[i]) {
+			part1++
+		}
+	}
 	//dbg("lines: %#v", lines)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
 
+}
+
+func getDirection(n, n1 int) int {
+
+	diff := n - n1
+	switch {
+	case diff < 0:
+		return -1
+	case diff > 0:
+		return 1
+	}
+
+	return 0
+}
+
+func isSafe(s string) bool {
+	// -1
+	direction := 0
+	digits := strings.Fields(s)
+	for i := 0; i < len(digits)-1; i++ {
+		n, _ := strconv.Atoi(digits[i])
+		n1, _ := strconv.Atoi(digits[i+1])
+		dir := getDirection(n, n1)
+		if direction == 0 {
+			direction = dir
+		}
+
+		if direction != dir {
+			return false
+		}
+
+		diff := utils.Abs(n - n1)
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+	return true
 }
