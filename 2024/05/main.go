3a0250ec8ff937d5c314c27f986efdb4b71738fa
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-05 18:23:15 +0100

    2024-05: part one

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"slices"
 	"strings"
+
+	"github.com/agimenez/adventofcode/utils"
 )
 
 var (
@@ -31,7 +34,48 @@ func main() {
 	}
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
-	//dbg("lines: %#v", lines)
+
+	inRules := true
+	rules := make(map[int][]int)
+	for _, l := range lines {
+		if l == "" {
+			inRules = false
+			dbg("Rules: %v", rules)
+			continue
+		}
+
+		if inRules {
+			parts := strings.Split(l, "|")
+			before := utils.ToInt(parts[0])
+			after := utils.ToInt(parts[1])
+			rules[before] = append(rules[before], after)
+		} else {
+			pages := utils.CSVToIntSlice(l, ",")
+			dbg("Pages: %v", pages)
+
+			// This should return -1 when page "a" goes before page "b", 1 otherwise
+			cmp := func(a, b int) int {
+				dbg("Comparing %v and %v (%v)", a, b, rules[b])
+				for _, v := range rules[b] {
+					dbg(" - %v", v)
+					if a == v {
+						dbg(" -    -> FOUND (%v before %v)", a, b)
+						return 1
+					}
+
+				}
+				return -1
+
+			}
+
+			if slices.IsSortedFunc(pages, cmp) {
+				dbg("%v is SORTED, returning %d", l, pages[len(pages)/2])
+				part1 += pages[len(pages)/2]
+			}
+
+		}
+
+	}
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
