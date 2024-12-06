b999124d67c9f91763052b1e64751cbc9e79c702
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-06 12:17:45 +0100

    2024-06: part one

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+
+	. "github.com/agimenez/adventofcode/utils"
 )
 
 var (
@@ -32,8 +35,59 @@ func main() {
 	lines := strings.Split(string(p), "\n")
 	lines = lines[:len(lines)-1]
 	//dbg("lines: %#v", lines)
+	var start Point
+	for y := 0; y < len(lines); y++ {
+		idx := strings.IndexByte(lines[y], '^')
+		if idx != -1 {
+			start = Point{idx, y}
+		}
+	}
+	visited := walkMap(lines, start, Point{0, 0}.Up())
+	part1 = len(visited)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
 
+}
+
+func printMap(m []string, cur, dir Point) {
+	if !debug {
+		return
+	}
+
+	for y, l := range m {
+		for x, c := range l {
+			p := Point{x, y}
+			if cur == p {
+				fmt.Printf("x")
+			} else {
+				fmt.Printf("%c", c)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func walkMap(m []string, start, dir Point) map[Point]bool {
+	visited := map[Point]bool{}
+	for {
+		visited[start] = true
+		next := start.Sum(dir)
+		dbg("* Point: %v, dir: %v, next: %v", start, dir, next)
+		c, inside := GetChInPoint(m, next)
+		if !inside {
+			break
+		}
+
+		if c == '#' {
+			dir = dir.Rotate90()
+			continue
+		} else {
+
+			start = next
+		}
+		printMap(m, start, dir)
+	}
+
+	return visited
 }
