c9ce7dc615bdbbded0396f252808296f983cbec5
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-09 12:21:25 +0100

    2024-09: part one

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -23,6 +23,7 @@ func dbg(fmt string, v ...interface{}) {
 func init() {
 	flag.BoolVar(&debug, "debug", false, "enable debug")
 }
+
 func main() {
 	flag.Parse()
 
@@ -31,11 +32,62 @@ func main() {
 	if err != nil {
 		panic("could not read input")
 	}
-	lines := strings.Split(string(p), "\n")
-	lines = lines[:len(lines)-1]
-	//dbg("lines: %#v", lines)
+	line := strings.Trim(string(p), "\n")
+	dbg("lines: %#v", line)
+	inFile := true
+	fileID := 0
+	out := []int{}
+	for _, c := range line {
+		id := fileID
+		if !inFile {
+			id = -1
+		}
+		for blocks := ToInt(string(c)); blocks > 0; blocks-- {
+			out = append(out, id)
+		}
+
+		if inFile {
+			fileID++
+		}
+
+		inFile = !inFile
+	}
+	dbg("out: %#v", out)
+	defrag := defrag(out)
+	dbg("defrag: %v", defrag)
+	part1 = checksum(defrag)
 
 	log.Printf("Part 1: %v\n", part1)
 	log.Printf("Part 2: %v\n", part2)
 
+}
+
+func defrag(disk []int) []int {
+	for cur, last := 0, len(disk)-1; cur < len(disk) && cur < last; cur++ {
+		if disk[cur] != -1 {
+			continue
+		}
+
+		for ; disk[last] == -1 && last > 0; last-- {
+		}
+		disk[cur] = disk[last]
+		disk[last] = -1
+		last--
+		dbg("disk: %v", disk[:cur])
+	}
+
+	return disk
+}
+
+func checksum(disk []int) int {
+	res := 0
+	for i := 0; i < len(disk); i++ {
+		if disk[i] == -1 {
+			break
+		}
+
+		res += i * disk[i]
+	}
+
+	return res
 }
