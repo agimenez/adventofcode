7578b01d75d56ba0fc614c3742c9690e25aab655
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-11 08:34:36 +0100

    2024-10: init

---
+++ 2024/10/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	tests := []struct {
+		in  []string
+		out int
+	}{
+		{
+			[]string{
+				"...0...",
+				"...1...",
+				"...2...",
+				"6543456",
+				"7.....7",
+				"8.....8",
+				"9.....9",
+			}, 2,
+		},
+		{
+			[]string{
+				"89010123",
+				"78121874",
+				"87430965",
+				"96549874",
+				"45678903",
+				"32019012",
+				"01329801",
+				"10456732",
+			}, 36,
+		},
+	}
+
+	for _, tt := range tests {
+		l := countTrailHeads(tt.in)
+		if l != tt.out {
+			t.Errorf("Test: got %v, expected %v", l, tt.out)
+		}
+	}
+
+}
