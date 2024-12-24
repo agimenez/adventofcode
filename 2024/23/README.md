90775792aaf37b444d37920bc6247fc42d190771
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-24 23:31:58 +0100

    2024-23: init

---
+++ 2024/23/README.md
@@ -0,0 +1,5 @@
+## 2024 - Day 23
+
+```
+curl https://adventofcode.com/2024/day/23
+```
---
+++ 2024/23/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	. "github.com/agimenez/adventofcode/utils"
+)
+
+var (
+	debug bool
+)
+
+func dbg(fmt string, v ...interface{}) {
+	if debug {
+		log.Printf(fmt, v...)
+	}
+}
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+}
+func main() {
+	flag.Parse()
+
+	part1, part2 := 0, 0
+	p, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
+	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+	//dbg("lines: %#v", lines)
+
+	var now time.Time
+	var dur [2]time.Duration
+
+	now = time.Now()
+	part1 = solve1(lines)
+	dur[0] = time.Since(now)
+
+	now = time.Now()
+	part2 = solve2(lines)
+	dur[1] = time.Since(now)
+
+	log.Printf("Part 1 (%v): %v\n", dur[0], part1)
+	log.Printf("Part 2 (%v): %v\n", dur[1], part2)
+
+}
+
+func solve1(s []string) int {
+	res := 0
+
+	return res
+}
+
+func solve2(s []string) int {
+	res := 0
+
+	return res
+}
