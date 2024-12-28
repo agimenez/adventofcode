3a19ca392ee97cbd5f75a7287e1a123e40943bed
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-28 20:24:01 +0100

    2016-02: p1

---
+++ 2016/02/README.md
@@ -0,0 +1,5 @@
+## 2016 - Day 2
+
+```
+curl https://adventofcode.com/2016/day/2
+```
---
+++ 2016/02/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	. "github.com/agimenez/adventofcode/utils"
+)
+
+var (
+	debug bool
+)
+
+func dbg(fmt string, v ...interface{}) {
+	if debug {
+		log.Printf(fmt, v...)
+	}
+}
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+}
+
+var grid = []string{
+	"123",
+	"456",
+	"789",
+}
+
+func main() {
+	flag.Parse()
+
+	p, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
+	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+	//dbg("lines: %#v", lines)
+
+	part1, part2, dur1, dur2 := solve(lines)
+	log.Printf("Part 1 (%v): %v\n", dur1, part1)
+	log.Printf("Part 2 (%v): %v\n", dur2, part2)
+
+}
+
+func solve(lines []string) (int, int, time.Duration, time.Duration) {
+	var now time.Time
+	var dur [2]time.Duration
+
+	now = time.Now()
+	part1 := solve1(lines)
+	dur[0] = time.Since(now)
+
+	now = time.Now()
+	part2 := solve2(lines)
+	dur[1] = time.Since(now)
+
+	return part1, part2, dur[0], dur[1]
+
+}
+
+func solve1(s []string) int {
+	res := 0
+	cur := Point{1, 1}
+	dirs := map[rune]Point{
+		'U': P0.Up(),
+		'R': P0.Right(),
+		'D': P0.Down(),
+		'L': P0.Left(),
+	}
+	curChar, _ := GetChInPoint(s, cur)
+	chars := []rune{}
+	for _, l := range s {
+		for _, c := range l {
+			nextPos := cur.Sum(dirs[c])
+			nextChar, ok := GetChInPoint(grid, nextPos)
+			if ok {
+				cur = nextPos
+				curChar = nextChar
+			}
+		}
+		dbg("Next: %c", curChar)
+		chars = append(chars, rune(curChar))
+	}
+	dbg("Chars: %v", string(chars))
+	res = ToInt(string(chars))
+
+	return res
+}
+
+func solve2(s []string) int {
+	res := 0
+
+	return res
+}
