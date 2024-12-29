a35f9de39c350aa1eb28e33a52dc8e5a74fa30dd
Alberto Gimenez <agimenez@sysvalve.es>
2024-12-29 21:56:06 +0100

    2016-05: p1

---
+++ 2016/05/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	// . "github.com/agimenez/adventofcode/utils"
+)
+
+var (
+	debug bool
+)
+
+func dbg(fmt string, v ...interface{}) {
+	if debug {
+		log.Printf(fmt, v...)
+	}
+}
+
+func init() {
+	flag.BoolVar(&debug, "debug", false, "enable debug")
+}
+func main() {
+	flag.Parse()
+
+	p, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic("could not read input")
+	}
+	lines := strings.Split(string(p), "\n")
+	lines = lines[:len(lines)-1]
+	//dbg("lines: %#v", lines)
+
+	part1, part2, dur1, dur2 := solve(lines)
+	log.Printf("Part 1 (%v): %v\n", dur1, part1)
+	log.Printf("Part 2 (%v): %v\n", dur2, part2)
+
+}
+
+func hackHash(secret string, zeros int) string {
+	i := 0
+	zeroCmp := strings.Repeat("0", zeros)
+	var pwd strings.Builder
+	for pwd.Len() < 8 {
+		padding := strconv.Itoa(i)
+		hash := md5.Sum([]byte(secret + padding))
+		str := hex.EncodeToString(hash[:])
+		if strings.HasPrefix(str, zeroCmp) {
+			pwd.WriteByte(str[5])
+			dbg("Found (%d): hash: %v, pwd: %v", i, str, pwd.String())
+		}
+		i++
+	}
+	dbg("FINAL: %d = %v", i, pwd.String())
+
+	return pwd.String()
+
+}
+
+func solve(lines []string) (string, int, time.Duration, time.Duration) {
+	var now time.Time
+	var dur [2]time.Duration
+
+	now = time.Now()
+	part1 := solve1(lines)
+	dur[0] = time.Since(now)
+
+	now = time.Now()
+	part2 := solve2(lines)
+	dur[1] = time.Since(now)
+
+	return part1, part2, dur[0], dur[1]
+
+}
+
+func solve1(s []string) string {
+	res := hackHash(s[0], 5)
+
+	return res
+}
+
+func solve2(s []string) int {
+	res := 0
+
+	return res
+}
