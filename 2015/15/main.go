cf94ebab19d347fbbdf74fc00363d5076ac81932
Alberto Gimenez <agimenez@sysvalve.es>
2025-12-27 13:08:07 +0100

    2015-15: p2

--- 2015/15/main.go
+++ 2015/15/main.go
@@ -115,5 +115,50 @@ func solve1(s []string) int {
 func solve2(s []string) int {
 	res := 0
 
+	ings := []Ingredient{}
+	for _, line := range s {
+		parts := strings.Split(line, ": ")
+
+		properties := strings.Split(parts[1], " ")
+		i := Ingredient{
+			capacity:   ToInt(properties[1][:len(properties[1])-1]),
+			durability: ToInt(properties[3][:len(properties[3])-1]),
+			flavor:     ToInt(properties[5][:len(properties[5])-1]),
+			texture:    ToInt(properties[7][:len(properties[7])-1]),
+			calories:   ToInt(properties[9]),
+		}
+
+		ings = append(ings, i)
+
+	}
+
+	// A bit hardcoded, can't bother to make it more general for more ingredients
+	for i := 0; i <= 100; i++ {
+		for j := 0; j <= 100-i; j++ {
+			for k := 0; k <= 100-i-j; k++ {
+				l := 100 - i - j - k
+				dbg("Trying: %v, %v, %v, %v", i, j, k, l)
+
+				counters := []int{i, j, k, l}
+				var capacity, durability, flavor, texture, calories int
+
+				for n, ing := range ings {
+					capacity += counters[n] * ing.capacity
+					durability += counters[n] * ing.durability
+					flavor += counters[n] * ing.flavor
+					texture += counters[n] * ing.texture
+					calories += counters[n] * ing.calories
+				}
+
+				if capacity < 0 || durability < 0 || flavor < 0 || texture < 0 || calories != 500 {
+					continue
+				}
+				score := capacity * durability * flavor * texture
+
+				res = Max(res, score)
+			}
+		}
+	}
+
 	return res
 }
